234ae47e0cab50801c7bdf15334ebbda819da074
mlucasdev <lucasmsantos819@gmail.com>
2023-11-07 20:51:48 -0300

    feat: ensure User call Email with correct value

--- src/domain/entities/user/user.ts
+++ src/domain/entities/user/user.ts
@@ -1,11 +1,15 @@
 import { right, type Either } from '@/shared/either'
 import { Name } from './value-objects/name/name'
+import { Email } from './value-objects/email/email'
 
 export class User {
-  private constructor (private readonly name: Name) {}
+  private constructor (private readonly name: Name) {
+    Object.freeze(this)
+  }
 
-  static create (data: { name: string }): Either<Error, User> {
+  static create (data: { name: string, email: string }): Either<Error, User> {
     const nameOrError = Name.create(data.name)
+    const emailOrError = Email.create(data.email)
     return right(
       new User(nameOrError.value as unknown as Name)
     )
