328b48bc3d75e20326933d670dd3cf7edd915aa1
mlucasdev <lucasmsantos819@gmail.com>
2023-11-07 20:53:56 -0300

    feat: ensure User call Password with correct value

--- src/domain/entities/user/user.ts
+++ src/domain/entities/user/user.ts
@@ -1,15 +1,18 @@
 import { right, type Either } from '@/shared/either'
 import { Name } from './value-objects/name/name'
 import { Email } from './value-objects/email/email'
+import { Password } from './value-objects/password/password'
+import type { UserDto } from './user-dto'
 
 export class User {
   private constructor (private readonly name: Name) {
     Object.freeze(this)
   }
 
-  static create (data: { name: string, email: string }): Either<Error, User> {
+  static create (data: UserDto): Either<Error, User> {
     const nameOrError = Name.create(data.name)
-    const emailOrError = Email.create(data.email)
+    Email.create(data.email)
+    Password.create(data.password)
     return right(
       new User(nameOrError.value as unknown as Name)
     )
