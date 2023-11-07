9c4dfd623d759c1ba6bc8e65699f52dd562051e1
mlucasdev <lucasmsantos819@gmail.com>
2023-11-07 20:51:53 -0300

    test: ensure User call Email with correct value

--- src/domain/entities/user/user.spec.ts
+++ src/domain/entities/user/user.spec.ts
@@ -1,14 +1,28 @@
 import { User } from './user'
+import type { UserDto } from './user-dto'
+import { Email } from './value-objects/email/email'
 import { Name } from './value-objects/name/name'
 
 const privateFactory = <T=any>(Cls: any, ...args: any[]): T => {
   return new Cls(...args)
 }
 
+const makeFakeUserDto = (): UserDto => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
 describe('User Entity', () => {
   it('Should call Name with correct value', () => {
-    const creteSpy = jest.spyOn(Name, 'create')
-    User.create({ name: 'any_name' })
-    expect(creteSpy).toHaveBeenCalledWith('any_name')
+    const createSpy = jest.spyOn(Name, 'create')
+    User.create(makeFakeUserDto())
+    expect(createSpy).toHaveBeenCalledWith('any_name')
+  })
+
+  it('Should call Email with correct value', () => {
+    const createSpy = jest.spyOn(Email, 'create')
+    User.create(makeFakeUserDto())
+    expect(createSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
