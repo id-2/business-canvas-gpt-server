5457f66a466f59cf2dd5434fb7818f8bf72e775a
mlucasdev <lucasmsantos819@gmail.com>
2023-11-07 21:05:46 -0300

    test: ensure User return InvalidNameError if Name return InvalidNameError

--- src/domain/entities/user/user.spec.ts
+++ src/domain/entities/user/user.spec.ts
@@ -1,8 +1,10 @@
+import { left, right } from '@/shared/either'
 import { User } from './user'
 import type { UserDto } from './user-dto'
 import { Email } from './value-objects/email/email'
 import { Name } from './value-objects/name/name'
 import { Password } from './value-objects/password/password'
+import { InvalidNameError } from './errors'
 
 const privateFactory = <T=any>(Cls: any, ...args: any[]): T => {
   return new Cls(...args)
@@ -15,10 +17,23 @@ const makeFakeUserDto = (): UserDto => ({
 })
 
 describe('User Entity', () => {
+  beforeAll(() => {
+    const fakeName = privateFactory<Name>(Name, 'any_name')
+    jest.spyOn(Name, 'create').mockReturnValue(right(fakeName))
+  })
+
   it('Should call Name with correct value', () => {
     const createSpy = jest.spyOn(Name, 'create')
     User.create(makeFakeUserDto())
     expect(createSpy).toHaveBeenCalledWith('any_name')
+  })
+
+  it('Should return InvalidNameError if Name return InvalidNameError', () => {
+    jest.spyOn(Name, 'create').mockReturnValueOnce(
+      left(new InvalidNameError('any_name'))
+    )
+    const sut = User.create(makeFakeUserDto())
+    expect(sut.value).toEqual(new InvalidNameError('any_name'))
   })
 
   it('Should call Email with correct value', () => {
