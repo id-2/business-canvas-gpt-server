5df4b9e15128319cb46dc1a50e3658dd6e921f69
mlucasdev <lucasmsantos819@gmail.com>
2023-11-08 18:42:29 -0300

    feat: ensure AddUserUseCase return EmailInUseError if FetchUserByEmailRepo return an UserModel

--- src/interactions/usecases/add-user-usecase.ts
+++ src/interactions/usecases/add-user-usecase.ts
@@ -2,6 +2,7 @@ import type { AddUserRes, AddUser } from '@/domain/contracts'
 import type { FetchUserByEmailRepo } from '../contracts/db'
 import { User, type UserDto } from '@/domain/entities/user'
 import { left, right } from '@/shared/either'
+import { EmailInUseError } from '@/domain/errors'
 
 export class AddUserUseCase implements AddUser {
   constructor (private readonly fetchUserByEmailRepo: FetchUserByEmailRepo) {}
@@ -11,7 +12,10 @@ export class AddUserUseCase implements AddUser {
     if (userResult.isLeft()) {
       return left(userResult.value)
     }
-    await this.fetchUserByEmailRepo.fetchByEmail(dto.email)
+    const user = await this.fetchUserByEmailRepo.fetchByEmail(dto.email)
+    if (user) {
+      return left(new EmailInUseError(dto.email))
+    }
     return right({ token: '' })
   }
 }
