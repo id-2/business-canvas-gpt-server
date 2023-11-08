406e387f19f698a8e8655eacad60dfc54237e4d5
mlucasdev <lucasmsantos819@gmail.com>
2023-11-08 18:35:39 -0300

    test: ensure AddUserUseCase call FetchUserByEmailRepo with correct email

--- src/interactions/usecases/add-user-usecase.spec.ts
+++ src/interactions/usecases/add-user-usecase.spec.ts
@@ -1,9 +1,10 @@
+import type { FetchUserByEmailRepo } from '../contracts/db'
+import type { UserModel } from '@/domain/models/db-models'
 import { User, type UserDto } from '@/domain/entities/user'
 import { AddUserUseCase } from './add-user-usecase'
 import { left, right } from '@/shared/either'
 
 jest.mock('@/domain/entities/user/user', () => ({
-  ...jest.requireActual('@/domain/entities/user/user'),
   User: {
     create: jest.fn(() => {
       return right({
@@ -21,13 +22,24 @@ const makeFakeUserDto = (): UserDto => ({
   password: 'any_password'
 })
 
+const makeFetchUserByEmailRepo = (): FetchUserByEmailRepo => {
+  class FetchUserByEmailRepoStub implements FetchUserByEmailRepo {
+    async fetchByEmail (email: string): Promise<null | UserModel> {
+      return await Promise.resolve(null)
+    }
+  }
+  return new FetchUserByEmailRepoStub()
+}
+
 interface SutTypes {
   sut: AddUserUseCase
+  fetchUserByEmailRepoStub: FetchUserByEmailRepo
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AddUserUseCase()
-  return { sut }
+  const fetchUserByEmailRepoStub = makeFetchUserByEmailRepo()
+  const sut = new AddUserUseCase(fetchUserByEmailRepoStub)
+  return { sut, fetchUserByEmailRepoStub }
 }
 
 describe('AddUser UseCase', () => {
@@ -45,5 +57,12 @@ describe('AddUser UseCase', () => {
     )
     const result = await sut.perform(makeFakeUserDto())
     expect(result.value).toEqual(new Error('any_message'))
+  })
+
+  it('Should call FetchUserByEmailRepo with correct email', async () => {
+    const { sut, fetchUserByEmailRepoStub } = makeSut()
+    const fetchByEmailSpy = jest.spyOn(fetchUserByEmailRepoStub, 'fetchByEmail')
+    await sut.perform(makeFakeUserDto())
+    expect(fetchByEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
