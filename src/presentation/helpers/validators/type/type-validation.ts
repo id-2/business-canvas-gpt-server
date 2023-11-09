752e578ee839d7a25d7e800adc6633f8b15ca3da
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 11:20:00 -0300

    feat: ensure TypeValidation return InvalidTypeError if field type is different from required array type

--- src/presentation/helpers/validators/type/type-validation.ts
+++ src/presentation/helpers/validators/type/type-validation.ts
@@ -2,7 +2,7 @@ import type { Validation } from '@/presentation/contracts'
 import { type Either, left, right } from '@/shared/either'
 import { InvalidTypeError } from '@/presentation/errors'
 
-type FieldType = 'string' | 'number' | 'boolean' | 'array'
+export type FieldType = 'string' | 'number' | 'boolean' | 'array'
 
 export class TypeValidation implements Validation {
   constructor (
@@ -11,6 +11,11 @@ export class TypeValidation implements Validation {
   ) {}
 
   validate (input: any): Either<Error, null> {
+    if (this.fieldType === 'array') {
+      if (!(input[this.fieldName] instanceof Array)) {
+        left(new InvalidTypeError(this.fieldName))
+      }
+    }
     if (typeof input[this.fieldName] !== this.fieldType) {
       return left(new InvalidTypeError(this.fieldName))
     }
