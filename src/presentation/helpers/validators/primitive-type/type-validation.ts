5a1fb065c1350ab024e0c166dc7fea5f7cb3cb31
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 10:53:07 -0300

    feat: ensure TypeValidation return right result if validation is a success

--- src/presentation/helpers/validators/primitive-type/type-validation.ts
+++ src/presentation/helpers/validators/primitive-type/type-validation.ts
@@ -1,14 +1,14 @@
-import { InvalidTypeError } from '@/presentation/errors'
 import type { Validation } from '@/presentation/contracts'
 import { type Either, left, right } from '@/shared/either'
+import { InvalidTypeError } from '@/presentation/errors'
 
 export type FieldType = 'string' | 'number' | 'boolean' | 'array'
 
 export class TypeValidation implements Validation {
   constructor (
     private readonly fieldName: string,
-    private readonly fieldType: FieldType) {
-  }
+    private readonly fieldType: FieldType
+  ) {}
 
   validate (input: any): Either<Error, null> {
     if (typeof input[this.fieldName] !== this.fieldType) {
