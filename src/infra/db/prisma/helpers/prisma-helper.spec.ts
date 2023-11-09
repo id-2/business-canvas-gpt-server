6439781e9aa9fc61a08cd1ccc74cc0781032aa07
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 17:03:03 -0300

    test: ensure PrismaHelper reconnect if Prisma down

---
+++ src/infra/db/prisma/helpers/prisma-helper.spec.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaHelper as sut } from './prisma-helper'
+
+let prisma: PrismaClient
+
+describe('Prisma Helper', () => {
+  it('Should reconnect if Prisma down', async () => {
+    prisma = await sut.getCli()
+    expect(prisma).toBeInstanceOf(PrismaClient)
+    await sut.disconnect()
+    prisma = await sut.getCli()
+    expect(prisma).toBeInstanceOf(PrismaClient)
+  })
+})
