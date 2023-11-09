b7bdc92dc63ed51c18ca636d228450e9ca507624
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 17:43:48 -0300

    fix: disconnect prisma after all tests

--- src/infra/db/prisma/helpers/prisma-helper.spec.ts
+++ src/infra/db/prisma/helpers/prisma-helper.spec.ts
@@ -4,6 +4,10 @@ import { PrismaHelper as sut } from './prisma-helper'
 let prisma: PrismaClient
 
 describe('Prisma Helper', () => {
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
   it('Should reconnect if Prisma down', async () => {
     prisma = await sut.getCli()
     expect(prisma).toBeInstanceOf(PrismaClient)
