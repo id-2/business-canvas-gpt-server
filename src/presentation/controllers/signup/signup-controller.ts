7365d13b32a7598b44111675df073314ad487dec
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 09:30:09 -0300

    feat: ensure SignUpController return 400 if Validation fails

--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,5 +1,6 @@
 import type { Controller, Validation } from '@/presentation/contracts'
 import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 
 export class SignUpController implements Controller {
   constructor (
@@ -7,7 +8,10 @@ export class SignUpController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const validationResult = this.validation.validate(httpRequest.body)
+    if (validationResult.isLeft()) {
+      return badRequest(validationResult.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }
