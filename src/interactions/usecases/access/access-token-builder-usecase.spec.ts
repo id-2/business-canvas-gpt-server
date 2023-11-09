50e7b2accd84677e22498c07d11e8965e5e5c49e
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 11:27:54 -0300

    test: ensure AccessTokenBuilderUseCase call Ecrypter with correct values

---
+++ src/interactions/usecases/access/access-token-builder-usecase.spec.ts
@@ -0,0 +1,34 @@
+import type { Encrypter, EncrypterDto } from '@/interactions/contracts/cryptography'
+import type { AccessTokenModel } from '@/domain/models/output-models'
+import { AccessTokenBuilderUseCase } from './access-token-builder-usecase'
+
+const makeAccessTokenEncrypter = (): Encrypter => {
+  class AccessTokenEcrypterStub implements Encrypter {
+    async encrypt (data: EncrypterDto): Promise<AccessTokenModel> {
+      return { token: 'any_token' }
+    }
+  }
+  return new AccessTokenEcrypterStub()
+}
+
+interface SutTypes {
+  sut: AccessTokenBuilderUseCase
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  const encrypterStub = makeAccessTokenEncrypter()
+  const sut = new AccessTokenBuilderUseCase(encrypterStub)
+  return { sut, encrypterStub }
+}
+
+describe('AccessTokenBuilder UseCase', () => {
+  it('Should call Ecrypter with correct values', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    await sut.perform('any_value')
+    expect(encryptSpy).toHaveBeenCalledWith({
+      value: 'any_value', expiresInHours: 48
+    })
+  })
+})
