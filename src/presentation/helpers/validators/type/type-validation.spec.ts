80e284894f87afeea6a902f759dbaff4a160996b
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 11:20:13 -0300

    test: ensure TypeValidation return InvalidTypeError if field type is different from required array type

--- src/presentation/helpers/validators/type/type-validation.spec.ts
+++ src/presentation/helpers/validators/type/type-validation.spec.ts
@@ -1,22 +1,30 @@
+import { type FieldType, TypeValidation } from './type-validation'
 import { InvalidTypeError } from '@/presentation/errors'
 import { left } from '@/shared/either'
-import { TypeValidation } from './type-validation'
 
-const makeSut = (): TypeValidation => {
-  return new TypeValidation('field', 'string')
+const makeSut = (fieldType: FieldType): TypeValidation => {
+  return new TypeValidation('field', fieldType)
 }
 
 describe('Type Validation', () => {
   it('Should return InvalidTypeError if field type is different from required type', () => {
-    const sut = makeSut()
+    const sut = makeSut('string')
     const result = sut.validate({
       field: 0, otherField: 'other_value'
     })
     expect(result).toEqual(left(new InvalidTypeError('field')))
   })
 
+  it('Should return InvalidTypeError if field type is different from required array type', () => {
+    const sut = makeSut('array')
+    const result = sut.validate({
+      field: 'any_value', otherField: 'other_value'
+    })
+    expect(result).toEqual(left(new InvalidTypeError('field')))
+  })
+
   it('Should return right result if validation is a success', () => {
-    const sut = makeSut()
+    const sut = makeSut('string')
     const result = sut.validate({
       field: 'any_value', otherField: 'other_value'
     })
