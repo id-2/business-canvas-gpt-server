80cc1fa98751ae66d3aa1b5baa555b0bcaf7bd07
mlucasdev <lucasmsantos819@gmail.com>
2023-11-09 10:18:37 -0300

    test: ensure RequiredFieldValidation return right result if input contain required field

--- src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field/required-field-validation.spec.ts
@@ -13,11 +13,20 @@ describe('RequiredField Validation', () => {
   })
 
   it('Should return MissinParamError if required field is empty on input', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut('field')
     const result = sut.validate({
       field: '',
       otherField: 'any_value'
     })
     expect(result.value).toEqual(new MissingParamError('field'))
+  })
+
+  it('Should return right result if input contain required field', () => {
+    const sut = makeSut('field')
+    const result = sut.validate({
+      otherField: 'any_value',
+      field: 'any_field'
+    })
+    expect(result.isRight()).toBe(true)
   })
 })
