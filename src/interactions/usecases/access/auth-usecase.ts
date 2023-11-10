0fb046fa6bd00c5629201cdb4b296c6bd4417d79
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 20:57:37 -0300

    feat: ensure AuthUseCase return InvalidEmailError if Email Value Object returns InvalidEmailError

--- src/interactions/usecases/access/auth-usecase.ts
+++ src/interactions/usecases/access/auth-usecase.ts
@@ -1,10 +1,13 @@
 import type { Auth, AuthDto, AuthRes } from '@/domain/contracts'
 import { Email } from '@/domain/entities/user/value-objects'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class AuthUseCase implements Auth {
   async perform (dto: AuthDto): Promise<AuthRes> {
-    Email.create(dto.email)
+    const emailResult = Email.create(dto.email)
+    if (emailResult.isLeft()) {
+      return left(emailResult.value)
+    }
     return right({ token: '' })
   }
 }
