8d4f1f10be2715505845a383aa35aa79b87dab21
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 12:35:34 -0300

    test: ensure UserPrismaRepo return null if findUnique() not found User

--- src/infra/db/prisma/user/user-prisma-repo.spec.ts
+++ src/infra/db/prisma/user/user-prisma-repo.spec.ts
@@ -35,7 +35,7 @@ describe('UserPrisma Repo', () => {
   })
 
   describe('add()', () => {
-    it('Should create new User', async () => {
+    it('Should create new User if create() is a success', async () => {
       const sut = new UserPrismaRepo()
       await sut.add(makeFakeUserModel())
       const user = await prismock.user.findUnique({ where: { id: 'any_id' } })
@@ -49,6 +49,12 @@ describe('UserPrisma Repo', () => {
       await prismock.user.create({ data: makeFakeUserModel() })
       const user = await sut.fetchByEmail('any_email@mail.com')
       expect(user).toEqual(makeFakeUserModel())
+    })
+
+    it('Should return null if findUnique() not found User', async () => {
+      const sut = new UserPrismaRepo()
+      const user = await sut.fetchByEmail('any_email@mail.com')
+      expect(user).toBeNull()
     })
   })
 })
