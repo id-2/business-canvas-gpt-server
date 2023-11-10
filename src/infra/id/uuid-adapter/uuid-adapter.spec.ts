9dddb13c0cdecd66bfd09c5592945037eaaec78e
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 13:00:59 -0300

    test: ensure UuidAdapter return an ID if uuid v4 is a success

--- src/infra/id/uuid-adapter/uuid-adapter.spec.ts
+++ src/infra/id/uuid-adapter/uuid-adapter.spec.ts
@@ -4,10 +4,20 @@ import * as uuid from 'uuid'
 jest.mock('uuid')
 
 describe('Uuid Adapter', () => {
-  it('Should call v4 uuid', () => {
+  beforeAll(() => {
+    jest.spyOn(uuid, 'v4').mockReturnValue('any_id')
+  })
+
+  it('Should call uuid v4', () => {
     const sut = new UuidAdapter()
     const v4Spy = jest.spyOn(uuid, 'v4')
     sut.build()
     expect(v4Spy).toHaveBeenCalled()
+  })
+
+  it('Should return an ID if uuid v4 is a success', () => {
+    const sut = new UuidAdapter()
+    const idModel = sut.build()
+    expect(idModel).toEqual({ id: 'any_id' })
   })
 })
