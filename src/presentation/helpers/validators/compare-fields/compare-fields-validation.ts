8315218b2e5efc9e99727f6758a7a66da2012e0f
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 13:46:08 -0300

    refactor: rename property fieldNameToCompare of CompareFieldsValidation

--- src/presentation/helpers/validators/compare-fields/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields/compare-fields-validation.ts
@@ -1,16 +1,16 @@
-import { type Either, left, right } from '@/shared/either'
 import type { Validation } from '@/presentation/contracts'
+import { type Either, left, right } from '@/shared/either'
 import { InvalidParamError } from '@/presentation/errors'
 
 export class CompareFieldsValidation implements Validation {
   constructor (
     private readonly fieldName: string,
-    private readonly fieldToCompareName: string
+    private readonly fieldNameToCompare: string
   ) {}
 
   validate (input: any): Either<Error, null> {
-    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
-      return left(new InvalidParamError(this.fieldToCompareName))
+    if (input[this.fieldName] !== input[this.fieldNameToCompare]) {
+      return left(new InvalidParamError(this.fieldNameToCompare))
     }
     return right(null)
   }
