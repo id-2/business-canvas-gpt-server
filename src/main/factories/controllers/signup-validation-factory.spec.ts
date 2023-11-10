a45e716d5b79606e28d37679be4e9bca55d08cd0
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 14:08:00 -0300

    test: ensure SignUpValidation Factory call ValidationComposite with all validations

---
+++ src/main/factories/controllers/signup-validation-factory.spec.ts
@@ -0,0 +1,25 @@
+import { type Validation } from '@/presentation/contracts'
+import { ValidationComposite } from '@/presentation/helpers/validators'
+import { signUpValidationFactory } from './signup-validation-factory'
+import { compareFieldsValidationFactory, onlyRequiredFieldsValidationFactory, requiredFieldValidationFactory, typeValidationFactory } from '../validations'
+
+jest.mock('@/presentation/helpers/validators/composite/validation-composite')
+
+describe('SignUpValidation Factory', () => {
+  it('Should call ValidationComposite with all validations', async () => {
+    signUpValidationFactory()
+    const validations: Validation[] = []
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFields) {
+      validations.push(
+        requiredFieldValidationFactory(field),
+        typeValidationFactory(field, 'string')
+      )
+    }
+    validations.push(
+      onlyRequiredFieldsValidationFactory(requiredFields),
+      compareFieldsValidationFactory('password', 'passwordConfirmation')
+    )
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
