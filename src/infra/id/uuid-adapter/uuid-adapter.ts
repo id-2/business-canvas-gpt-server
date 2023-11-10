7481febe0ceaf982307ce2e355ad5df816960977
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 12:58:07 -0300

    feat: ensure UuidAdapter call v4 uuid

---
+++ src/infra/id/uuid-adapter/uuid-adapter.ts
@@ -0,0 +1,10 @@
+import type { IdModel } from '@/domain/models/output-models'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import * as uuid from 'uuid'
+
+export class UuidAdapter implements IdBuilder {
+  build (): IdModel {
+    uuid.v4()
+    return { id: '' }
+  }
+}
