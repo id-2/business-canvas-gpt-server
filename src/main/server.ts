0e03a4252aeb6b4681db94c2c0f4e26b23757b73
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 18:34:48 -0300

    feat: connect with prisma before server run

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,12 @@
 import 'module-alias/register'
 import app from './configs/app'
 import env from './configs/env'
+import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
 
-app.listen(env.port, () => {
-  console.log(`Server running at http://${env.host}:${env.port}`)
-})
+PrismaHelper.connect()
+  .then(() => {
+    app.listen(env.port, () => {
+      console.log(`Server running at http://${env.host}:${env.port}`)
+    })
+  })
+  .catch(console.error)
