a4066a0b10f3ff08c6806d6a28574873a2e37e81
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 18:05:57 -0300

    chore: add test environment with prisma, pg and jest

---
+++ src/main/configs/prisma-jest/prisma-environment-jest.ts
@@ -0,0 +1,40 @@
+import type { JestEnvironmentConfig, EnvironmentContext } from '@jest/environment'
+import NodeEnvironment from 'jest-environment-node'
+import { v4 as uuid } from 'uuid'
+import { execSync } from 'child_process'
+import { resolve } from 'path'
+import { Client } from 'pg'
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: resolve(__dirname, '.env.test')
+})
+
+class CustomEnvironment extends NodeEnvironment {
+  private readonly schema: string
+  private readonly connectionString: string
+
+  constructor (config: JestEnvironmentConfig, context: EnvironmentContext) {
+    super(config, context)
+    this.schema = `code_schema_${uuid()}`
+    this.connectionString = `${process.env.DATABASE_URL}${this.schema}`
+  }
+
+  async setup (): Promise<void> {
+    process.env.DATABASE_URL = this.connectionString
+    this.global.process.env.DATABASE_URL = this.connectionString
+    execSync('npx prisma db push')
+  }
+
+  async teardown (): Promise<void> {
+    const client = new Client({
+      connectionString: this.connectionString
+    })
+
+    await client.connect()
+    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`)
+    await client.end()
+  }
+}
+
+export default CustomEnvironment
