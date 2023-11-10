8eff24272c005fb94333e7e6e49019c3e472f856
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 14:07:53 -0300

    feat: ensure SignUpValidation Factory call ValidationComposite with all validations

---
+++ src/main/factories/controllers/signup-validation-factory.ts
@@ -0,0 +1,18 @@
+import { type Validation } from '@/presentation/contracts'
+import { compareFieldsValidationFactory, onlyRequiredFieldsValidationFactory, requiredFieldValidationFactory, typeValidationFactory, validationCompositeFactory } from '../validations'
+
+export const signUpValidationFactory = (): Validation => {
+  const validations: Validation[] = []
+  const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+  for (const field of requiredFields) {
+    validations.push(
+      requiredFieldValidationFactory(field),
+      typeValidationFactory(field, 'string')
+    )
+  }
+  validations.push(
+    onlyRequiredFieldsValidationFactory(requiredFields),
+    compareFieldsValidationFactory('password', 'passwordConfirmation')
+  )
+  return validationCompositeFactory(validations)
+}
