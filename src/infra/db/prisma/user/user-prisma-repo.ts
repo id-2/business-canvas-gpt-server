ec1452601444f06238c1ad32fb400130bcd6aeb6
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 12:33:51 -0300

    feat: ensure UserPrismaRepo return an User if findUnique() is a success

--- src/infra/db/prisma/user/user-prisma-repo.ts
+++ src/infra/db/prisma/user/user-prisma-repo.ts
@@ -1,9 +1,16 @@
 import type { UserModel } from '@/domain/models/db-models'
-import type { AddUserRepo } from '@/interactions/contracts/db'
+import type { AddUserRepo, FetchUserByEmailRepo } from '@/interactions/contracts/db'
 import { PrismaHelper as prisma } from '../helpers/prisma-helper'
 
-export class UserPrismaRepo implements AddUserRepo {
+export class UserPrismaRepo implements AddUserRepo, FetchUserByEmailRepo {
   async add (data: UserModel): Promise<void> {
     await (await prisma.getCli()).user.create({ data })
+  }
+
+  async fetchByEmail (email: string): Promise<null | UserModel> {
+    const userOrNull = await (await prisma.getCli()).user.findUnique({
+      where: { email }
+    })
+    return userOrNull
   }
 }
