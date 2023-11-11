843f2db8c1102a8ce2389eca88feadd4baf173c1
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 23:30:39 -0300

    feat: ensure FetchQuestionsUseCase return QuestionsNotFoundError if FetchAllQuestionsRepo returns empty list

--- src/interactions/usecases/question/fetch-questions-usecase.ts
+++ src/interactions/usecases/question/fetch-questions-usecase.ts
@@ -1,12 +1,16 @@
 import type { FetchQuestions, FetchQuestionsRes } from '@/domain/contracts'
 import type { FetchAllQuestionsRepo } from '@/interactions/contracts/db'
-import { right } from '@/shared/either'
+import { QuestionsNotFoundError } from '@/domain/errors'
+import { left, right } from '@/shared/either'
 
 export class FetchQuestionsUseCase implements FetchQuestions {
   constructor (private readonly fetchAllQuestionsRepo: FetchAllQuestionsRepo) {}
 
   async perform (): Promise<FetchQuestionsRes> {
-    await this.fetchAllQuestionsRepo.fetchAll()
+    const questions = await this.fetchAllQuestionsRepo.fetchAll()
+    if (questions.length === 0) {
+      return left(new QuestionsNotFoundError())
+    }
     return right([])
   }
 }
