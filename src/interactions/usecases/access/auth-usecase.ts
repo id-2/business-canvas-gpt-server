07ada6356620c951174a75f9c684ac14f2a08490
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 21:03:01 -0300

    feat: ensure AuthUseCase call FetchUserByEmailRepo with correct email

--- src/interactions/usecases/access/auth-usecase.ts
+++ src/interactions/usecases/access/auth-usecase.ts
@@ -1,13 +1,17 @@
 import type { Auth, AuthDto, AuthRes } from '@/domain/contracts'
+import type { FetchUserByEmailRepo } from '@/interactions/contracts/db'
 import { Email } from '@/domain/entities/user/value-objects'
 import { left, right } from '@/shared/either'
 
 export class AuthUseCase implements Auth {
+  constructor (private readonly fetchUserByEmailRepo: FetchUserByEmailRepo) {}
+
   async perform (dto: AuthDto): Promise<AuthRes> {
     const emailResult = Email.create(dto.email)
     if (emailResult.isLeft()) {
       return left(emailResult.value)
     }
+    await this.fetchUserByEmailRepo.fetchByEmail(dto.email)
     return right({ token: '' })
   }
 }
