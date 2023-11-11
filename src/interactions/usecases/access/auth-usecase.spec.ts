2c9bb0804c6f6126235e319fa1249456f9263a40
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 21:03:06 -0300

    test: ensure AuthUseCase call FetchUserByEmailRepo with correct email

--- src/interactions/usecases/access/auth-usecase.spec.ts
+++ src/interactions/usecases/access/auth-usecase.spec.ts
@@ -1,3 +1,5 @@
+import type { FetchUserByEmailRepo } from '@/interactions/contracts/db'
+import type { UserModel } from '@/domain/models/db-models'
 import type { AuthDto } from '@/domain/contracts'
 import { AuthUseCase } from './auth-usecase'
 import { Email } from '@/domain/entities/user/value-objects'
@@ -15,13 +17,34 @@ const makeFakeAuthDto = (): AuthDto => ({
   password: 'any_password'
 })
 
+const makeFakeUserModel = (): UserModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password',
+  role: 'user',
+  createdAt: new Date(),
+  updatedAt: new Date()
+})
+
+const makeFetchUserByEmailRepo = (): FetchUserByEmailRepo => {
+  class FetchUserByEmailRepoStub implements FetchUserByEmailRepo {
+    async fetchByEmail (email: string): Promise<null | UserModel> {
+      return await Promise.resolve(makeFakeUserModel())
+    }
+  }
+  return new FetchUserByEmailRepoStub()
+}
+
 interface SutTypes {
   sut: AuthUseCase
+  fetchUserByEmailRepoStub: FetchUserByEmailRepo
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthUseCase()
-  return { sut }
+  const fetchUserByEmailRepoStub = makeFetchUserByEmailRepo()
+  const sut = new AuthUseCase(fetchUserByEmailRepoStub)
+  return { sut, fetchUserByEmailRepoStub }
 }
 
 describe('Auth UseCase', () => {
@@ -48,5 +71,12 @@ describe('Auth UseCase', () => {
     })
     const promise = sut.perform(makeFakeAuthDto())
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call FetchUserByEmailRepo with correct email', async () => {
+    const { sut, fetchUserByEmailRepoStub } = makeSut()
+    const fetchByEmailSpy = jest.spyOn(fetchUserByEmailRepoStub, 'fetchByEmail')
+    await sut.perform(makeFakeAuthDto())
+    expect(fetchByEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
