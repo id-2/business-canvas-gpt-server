445a10d523280bb5b21496089b08909927ec3b69
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 21:07:20 -0300

    feat: ensure AuthUseCase return InvalidCredentialsError if FetchUserByEmailRepo returns null

--- src/interactions/usecases/access/auth-usecase.ts
+++ src/interactions/usecases/access/auth-usecase.ts
@@ -1,7 +1,8 @@
 import type { Auth, AuthDto, AuthRes } from '@/domain/contracts'
 import type { FetchUserByEmailRepo } from '@/interactions/contracts/db'
-import { Email } from '@/domain/entities/user/value-objects'
+import { InvalidCredentialsError } from '@/domain/errors'
 import { left, right } from '@/shared/either'
+import { Email } from '@/domain/entities/user/value-objects'
 
 export class AuthUseCase implements Auth {
   constructor (private readonly fetchUserByEmailRepo: FetchUserByEmailRepo) {}
@@ -11,7 +12,10 @@ export class AuthUseCase implements Auth {
     if (emailResult.isLeft()) {
       return left(emailResult.value)
     }
-    await this.fetchUserByEmailRepo.fetchByEmail(dto.email)
+    const user = await this.fetchUserByEmailRepo.fetchByEmail(dto.email)
+    if (!user) {
+      return left(new InvalidCredentialsError())
+    }
     return right({ token: '' })
   }
 }
