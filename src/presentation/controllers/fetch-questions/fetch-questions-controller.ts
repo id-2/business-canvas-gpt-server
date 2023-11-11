2f687b5a3076a3274739cf12affefdac8e25321e
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 23:53:57 -0300

    feat: ensure FetchQuestionsController return 200 if FetchQuestions is a success

--- src/presentation/controllers/fetch-questions/fetch-questions-controller.ts
+++ src/presentation/controllers/fetch-questions/fetch-questions-controller.ts
@@ -1,7 +1,7 @@
 import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
 import type { FetchQuestions } from '@/domain/contracts'
 import type { Controller } from '@/presentation/contracts'
-import { notFound, serverError } from '@/presentation/helpers/http/http-helpers'
+import { notFound, ok, serverError } from '@/presentation/helpers/http/http-helpers'
 
 export class FetchQuestionsController implements Controller {
   constructor (private readonly fetchQuestions: FetchQuestions) {}
@@ -12,7 +12,7 @@ export class FetchQuestionsController implements Controller {
       if (fetchQuestionsResult.isLeft()) {
         return notFound(fetchQuestionsResult.value)
       }
-      return { statusCode: 0, body: '' }
+      return ok(fetchQuestionsResult.value)
     } catch (error: any) {
       return serverError(error)
     }
