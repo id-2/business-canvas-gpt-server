881a2253978550a6048347aedb6ca2c269ba5e5f
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 23:52:31 -0300

    feat: ensure FetchQuestionsController return 500 if FetchQuestions throws

--- src/presentation/controllers/fetch-questions/fetch-questions-controller.ts
+++ src/presentation/controllers/fetch-questions/fetch-questions-controller.ts
@@ -1,16 +1,20 @@
 import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
 import type { FetchQuestions } from '@/domain/contracts'
 import type { Controller } from '@/presentation/contracts'
-import { notFound } from '@/presentation/helpers/http/http-helpers'
+import { notFound, serverError } from '@/presentation/helpers/http/http-helpers'
 
 export class FetchQuestionsController implements Controller {
   constructor (private readonly fetchQuestions: FetchQuestions) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const fetchQuestionsResult = await this.fetchQuestions.perform()
-    if (fetchQuestionsResult.isLeft()) {
-      return notFound(fetchQuestionsResult.value)
+    try {
+      const fetchQuestionsResult = await this.fetchQuestions.perform()
+      if (fetchQuestionsResult.isLeft()) {
+        return notFound(fetchQuestionsResult.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }
