22597f7b8681368b420ccb5a4a25eaab46f6fa21
mlucasdev <lucasmsantos819@gmail.com>
2023-11-10 22:28:53 -0300

    feat: ensure login route return 200 on login

--- src/main/routes/access/access-routes.ts
+++ src/main/routes/access/access-routes.ts
@@ -1,7 +1,9 @@
 import type { Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
 import { signUpControllerFactory } from '@/main/factories/controllers/signup/signup-controller-factory'
+import { loginControllerFactory } from '@/main/factories/controllers/login/login-controller-factory'
 
 export default async (router: Router): Promise<void> => {
   router.post('/signup', adaptRoute(signUpControllerFactory()))
+  router.post('/login', adaptRoute(loginControllerFactory()))
 }
