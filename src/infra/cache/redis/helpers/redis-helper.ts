4485a85ce518b4cec804fd8928d8e652f4037608
mlucasdev <lucasmsantos819@gmail.com>
2023-11-11 16:18:03 -0300

    feat: ensure RedisHelper create only once Redis instance

---
+++ src/infra/cache/redis/helpers/redis-helper.ts
@@ -0,0 +1,31 @@
+import { Redis } from 'ioredis'
+import env from '@/main/configs/env'
+
+export class RedisHelper {
+  private static cli: Redis | null
+
+  static connect (): void {
+    if (!RedisHelper.cli) {
+      this.cli = new Redis({
+        host: env.redisHost,
+        port: Number(env.redisPort),
+        password: env.redisPassword
+      })
+    }
+  }
+
+  static getInstance (): Redis {
+    if (!this.cli) {
+      RedisHelper.connect()
+      return this.cli as unknown as Redis
+    }
+    return this.cli
+  }
+
+  static async disconnect (): Promise<void> {
+    if (this.cli) {
+      await this.cli.quit()
+      this.cli = null
+    }
+  }
+}
--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -5,5 +5,8 @@ dotenv.config()
 export default {
   jwtSecretKey: process.env.JWT_SECRET_KEY ?? 'any_secret',
   port: process.env.PORT ?? 5050,
-  host: process.env.HOST ?? '127.0.0.1'
+  host: process.env.HOST ?? '127.0.0.1',
+  redisHost: process.env.REDIS_HOST ?? '0.0.0.0',
+  redisPort: process.env.REDIS_PORT ?? '6379',
+  redisPassword: process.env.REDIS_PASSWORD ?? ''
 }
