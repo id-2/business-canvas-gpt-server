934881c14c639a34c7b3f7ad83a6d9f1cfa218fd
mlucasdev <lucasmsantos819@gmail.com>
2023-11-12 20:53:37 -0300

    test: ensure Question Entity call LocationOrTargetAudience

--- src/domain/entities/question/question.spec.ts
+++ src/domain/entities/question/question.spec.ts
@@ -1,11 +1,5 @@
 import { Question } from './question'
-import { BusinessDescription, TypeOfBusiness } from './value-objects'
-
-jest.mock('@/domain/entities/question/value-objects/business-description/business-description', () => ({
-  BusinessDescription: {
-    create: jest.fn().mockReturnValue('any_business_description')
-  }
-}))
+import { BusinessDescription, LocationOrTargetAudience, TypeOfBusiness } from './value-objects'
 
 jest.mock('@/domain/entities/question/value-objects/type-of-business/type-of-business', () => ({
   TypeOfBusiness: {
@@ -13,9 +7,27 @@ jest.mock('@/domain/entities/question/value-objects/type-of-business/type-of-bus
   }
 }))
 
+jest.mock('@/domain/entities/question/value-objects/location-or-target-audience/location-or-target-audience', () => ({
+  LocationOrTargetAudience: {
+    create: jest.fn().mockReturnValue('any_location_or_target_audience')
+  }
+}))
+
+jest.mock('@/domain/entities/question/value-objects/business-description/business-description', () => ({
+  BusinessDescription: {
+    create: jest.fn().mockReturnValue('any_business_description')
+  }
+}))
+
 describe('Question Entity', () => {
   it('Should call TypeOfBusiness', () => {
     const createSpy = jest.spyOn(TypeOfBusiness, 'create')
+    Question.createMany()
+    expect(createSpy).toHaveBeenCalled()
+  })
+
+  it('Should call LocationOrTargetAudience', () => {
+    const createSpy = jest.spyOn(LocationOrTargetAudience, 'create')
     Question.createMany()
     expect(createSpy).toHaveBeenCalled()
   })
