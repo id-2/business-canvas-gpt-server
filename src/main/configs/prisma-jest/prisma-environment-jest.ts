511f4e7c1b14f2f7928cba11fb0124ad8791e7ba
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 20:22:10 -0300

    fix: remove error when creating schema with duplicate name

--- src/main/configs/prisma-jest/prisma-environment-jest.ts
+++ src/main/configs/prisma-jest/prisma-environment-jest.ts
@@ -10,29 +10,30 @@ dotenv.config({
 })
 
 class CustomEnvironment extends NodeEnvironment {
-  private readonly schema: string
-  private readonly connectionString: string
+  private static schema: string | null = null
+  private static connectionString: string | undefined = undefined
 
   constructor (config: JestEnvironmentConfig, context: EnvironmentContext) {
     super(config, context)
-    const random = Math.random().toString()
-    const randomString = random.split('.')[1]
-    this.schema = `code_schema_${randomString}`
-    this.connectionString = `${process.env.DATABASE_URL}${this.schema}`
+    if (!CustomEnvironment.schema) {
+      const randomString = Math.random().toString().substring(2, 10)
+      CustomEnvironment.schema = `code_schema_${randomString}`
+      CustomEnvironment.connectionString = `${process.env.DATABASE_URL}?schema=${CustomEnvironment.schema}`
+    }
   }
 
   async setup (): Promise<void> {
-    process.env.DATABASE_URL = this.connectionString
-    this.global.process.env.DATABASE_URL = this.connectionString
+    process.env.DATABASE_URL = CustomEnvironment.connectionString
+    this.global.process.env.DATABASE_URL = CustomEnvironment.connectionString
     execSync('npx prisma db push')
   }
 
   async teardown (): Promise<void> {
     const client = new Client({
-      connectionString: this.connectionString
+      connectionString: CustomEnvironment.connectionString
     })
     await client.connect()
-    await client.query(`DROP SCHEMA IF EXISTS "${this.schema}" CASCADE`)
+    await client.query(`DROP SCHEMA IF EXISTS "${CustomEnvironment.schema}" CASCADE`)
     await client.end()
   }
 }
