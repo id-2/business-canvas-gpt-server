c7b8fddf76d2048ca857fbba3042764e54f5a202
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 15:13:10 -0300

    feat: esnure ReplyQuestionsUseCase throw QuestionsNotFoundError if FetchAllQuestionsRepo returns null

--- src/interactions/usecases/question/reply-questions/reply-questions-usecase.ts
+++ src/interactions/usecases/question/reply-questions/reply-questions-usecase.ts
@@ -1,10 +1,14 @@
 import type { ReplyQuestions } from '@/domain/contracts'
+import { QuestionsNotFoundError } from '@/domain/errors'
 import type { FetchAllQuestionsRepo } from '@/interactions/contracts/db'
 
 export class ReplyQuestionsUseCase implements ReplyQuestions {
   constructor (private readonly fetchAllQuestionsRepo: FetchAllQuestionsRepo) {}
 
   async perform (): Promise<void> {
-    await this.fetchAllQuestionsRepo.fetchAll()
+    const questions = await this.fetchAllQuestionsRepo.fetchAll()
+    if (!questions) {
+      throw new QuestionsNotFoundError()
+    }
   }
 }
