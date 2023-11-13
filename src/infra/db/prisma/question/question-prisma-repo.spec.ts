839390c6fac01e1a93383bc63b2e5f0a13db3a65
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 15:51:12 -0300

    test: ensure QuestionPrismaRepo adreturn all questions with alternatives on success

--- src/infra/db/prisma/question/question-prisma-repo.spec.ts
+++ src/infra/db/prisma/question/question-prisma-repo.spec.ts
@@ -45,15 +45,32 @@ describe('QuestionPrisma Repo', () => {
     await prismock.$disconnect()
   })
 
-  it('Should create many Questions with Alternatives on success', async () => {
-    const sut = new QuestionPrismaRepo()
-    await sut.addMany(makeFakeQuestionsModel())
-    const questions = await prismock.question.findMany()
-    const alternatives = await prismock.alternative.findMany()
-    expect(questions).toEqual([
-      { id: 'any_id', content: 'any_content' },
-      { id: 'other_id', content: 'other_content' }
-    ])
-    expect(alternatives).toEqual(makeFakeAlternativesModel())
+  describe('addMany()', () => {
+    it('Should create many Questions with Alternatives on success', async () => {
+      const sut = new QuestionPrismaRepo()
+      await sut.addMany(makeFakeQuestionsModel())
+      const questions = await prismock.question.findMany()
+      const alternatives = await prismock.alternative.findMany()
+      expect(questions).toEqual([
+        { id: 'any_id', content: 'any_content' },
+        { id: 'other_id', content: 'other_content' }
+      ])
+      expect(alternatives).toEqual(makeFakeAlternativesModel())
+    })
+  })
+
+  describe('fetchAll()', () => {
+    it('Should return all questions with alternatives on success', async () => {
+      const sut = new QuestionPrismaRepo()
+      const data: QuestionModel[] = makeFakeQuestionsModel().map(
+        ({ id, content }) => ({ id, content })
+      )
+      await prismock.question.createMany({ data })
+      await prismock.alternative.createMany({ data: makeFakeAlternativesModel() })
+      const questions = await sut.fetchAll() as QuestionModel[]
+      const alternatives = await prismock.alternative.findMany()
+      expect(questions).toEqual(makeFakeQuestionsModel())
+      expect(alternatives).toEqual(makeFakeAlternativesModel())
+    })
   })
 })
