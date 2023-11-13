7db7077d273c05c57b22ec51efc53f46b5b74437
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 15:19:47 -0300

    feat: esnure ReplyQuestionsUseCase call AddManyQuestionsRepo with correct values

--- src/interactions/usecases/question/reply-questions/reply-questions-usecase.ts
+++ src/interactions/usecases/question/reply-questions/reply-questions-usecase.ts
@@ -1,14 +1,18 @@
+import type { AddManyQuestionsRepo, FetchAllQuestionsRepo } from '@/interactions/contracts/db'
 import type { ReplyQuestions } from '@/domain/contracts'
 import { QuestionsNotFoundError } from '@/domain/errors'
-import type { FetchAllQuestionsRepo } from '@/interactions/contracts/db'
 
 export class ReplyQuestionsUseCase implements ReplyQuestions {
-  constructor (private readonly fetchAllQuestionsRepo: FetchAllQuestionsRepo) {}
+  constructor (
+    private readonly fetchAllQuestionsRepo: FetchAllQuestionsRepo,
+    private readonly addManyQuestionsRepo: AddManyQuestionsRepo
+  ) {}
 
   async perform (): Promise<void> {
     const questions = await this.fetchAllQuestionsRepo.fetchAll()
     if (!questions || questions.length === 0) {
       throw new QuestionsNotFoundError()
     }
+    await this.addManyQuestionsRepo.addMany(questions)
   }
 }
