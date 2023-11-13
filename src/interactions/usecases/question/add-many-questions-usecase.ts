7933d98368bb7d38a26588a721e38d3f9809e5d5
mlucasdev <lucasmsantos819@gmail.com>
2023-11-12 21:42:08 -0300

    feat: ensure AddManyQuestionsUseCase call IdBuilder

--- src/interactions/usecases/question/add-many-questions-usecase.ts
+++ src/interactions/usecases/question/add-many-questions-usecase.ts
@@ -1,8 +1,12 @@
 import type { AddManyQuestions } from '@/domain/contracts'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
 import { Question } from '@/domain/entities/question/question'
 
 export class AddManyQuestionsUseCase implements AddManyQuestions {
+  constructor (private readonly idBuilder: IdBuilder) {}
+
   async perform (): Promise<void> {
+    this.idBuilder.build()
     Question.createMany()
   }
 }
