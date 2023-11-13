55e2a421691b25cd02e7bdb37de7e1227e88e934
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 14:45:32 -0300

    feat: connect with Prisma before adding questions and disconnects after adding

--- src/main/seeds/add-many-questions-prisma-seed.ts
+++ src/main/seeds/add-many-questions-prisma-seed.ts
@@ -1,10 +1,13 @@
 import 'module-alias/register'
 import { addManyQuestionsUseCaseFactory } from '../factories/usecases/add-many-questions-usecase-factory'
+import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
 
-export const addManyQuestionsPrismaSeed = async (): Promise<void> => {
+const addManyQuestionsPrismaSeed = async (): Promise<void> => {
+  await PrismaHelper.connect()
   await addManyQuestionsUseCaseFactory().perform()
 }
 
-addManyQuestionsPrismaSeed()
+export default addManyQuestionsPrismaSeed()
   .then(() => { console.log('Questions added successfully') })
   .catch(console.error)
+  .finally(async () => { await PrismaHelper.disconnect() })
