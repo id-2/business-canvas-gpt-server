a639769715c677637a8da96995f103e829fb7914
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 19:20:49 -0300

    feat: ensure addAndReplyQuestionsSeed() reply all Questions in Cache with Redis

--- src/main/seeds/add-and-reply-questions-seed.ts
+++ src/main/seeds/add-and-reply-questions-seed.ts
@@ -1,10 +1,12 @@
 import 'module-alias/register'
 import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
 import { addManyQuestionsUseCaseFactory } from '../factories/usecases/add-many-questions-usecase-factory'
+import { replyQuestionsUseCaseFactory } from '../factories/usecases/reply-questions-usecase-factory'
 
 const addAndReplyQuestionsSeed = async (): Promise<void> => {
   await PrismaHelper.connect()
   await addManyQuestionsUseCaseFactory().perform()
+  await replyQuestionsUseCaseFactory().perform()
 }
 
 export default addAndReplyQuestionsSeed()
