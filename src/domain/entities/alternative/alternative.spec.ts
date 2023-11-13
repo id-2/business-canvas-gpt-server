80680600ede699aa5fcdf6a1c57d516db95460dd
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 11:59:27 -0300

    test: ensure Alternative Entity return the correct description for a Alternative

--- src/domain/entities/alternative/alternative.spec.ts
+++ src/domain/entities/alternative/alternative.spec.ts
@@ -1,6 +1,12 @@
 import { Alternative } from './alternative'
 
 describe('Alternative Entity', () => {
+  it('Should return the correct description for a Alternative', () => {
+    const alternative = Alternative.create('any_description')
+    const result = Alternative.getDescription(alternative)
+    expect(result).toBe('any_description')
+  })
+
   it('Should return new Alternative', () => {
     const sut = Alternative.create('any_description')
     expect(sut).toEqual({ description: 'any_description' })
