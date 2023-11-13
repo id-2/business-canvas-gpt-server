f01ed1b7aefe66c8ca93241acc00080919b0cf4b
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 00:40:33 -0300

    feat: run addManyQuestionsPrismaSeed()

--- src/main/seeds/add-many-questions-prisma-seed.ts
+++ src/main/seeds/add-many-questions-prisma-seed.ts
@@ -1,5 +1,10 @@
+import 'module-alias/register'
 import { addManyQuestionsUseCaseFactory } from '../factories/usecases/add-many-questions-usecase-factory'
 
 export const addManyQuestionsPrismaSeed = async (): Promise<void> => {
   await addManyQuestionsUseCaseFactory().perform()
 }
+
+addManyQuestionsPrismaSeed()
+  .then(() => { console.log('Questions added successfully') })
+  .catch(console.error)
