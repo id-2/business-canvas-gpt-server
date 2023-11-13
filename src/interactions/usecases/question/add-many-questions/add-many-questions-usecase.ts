54434ce20c27527e707e35b27aa381e4a01f2f5d
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 13:08:20 -0300

    feat: ensure AddManyQuestionsUseCase call AddManyQuestionsRepo with correct Question and Alternatives

--- src/interactions/usecases/question/add-many-questions/add-many-questions-usecase.ts
+++ src/interactions/usecases/question/add-many-questions/add-many-questions-usecase.ts
@@ -1,7 +1,8 @@
 import type { AddManyQuestions } from '@/domain/contracts'
-import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import type { AlternativeModel, QuestionModel } from '@/domain/models/db-models'
 import type { AddManyQuestionsRepo } from '@/interactions/contracts/db'
-import type { QuestionModel } from '@/domain/models/db-models'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import { Alternative } from '@/domain/entities/alternative/alternative'
 import { Question } from '@/domain/entities/question/question'
 
 export class AddManyQuestionsUseCase implements AddManyQuestions {
@@ -12,13 +13,36 @@ export class AddManyQuestionsUseCase implements AddManyQuestions {
 
   async perform (): Promise<void> {
     const questions = Question.createMany()
+    const questionsModel = this.getQuestionsModel(questions)
+    await this.addManyQuestionsRepo.addMany(questionsModel)
+  }
+
+  private getQuestionsModel (questions: Question[]): QuestionModel[] {
     const questionsModel: QuestionModel[] = []
     for (const question of questions) {
-      questionsModel.push({
-        id: this.idBuilder.build().id,
-        content: Question.getQuestion(question)?.content as string
-      })
+      const questionId = this.idBuilder.build().id
+      const alternatives = Question.getQuestion(question)?.alternatives
+      const alternativesModel: AlternativeModel[] = []
+      if (alternatives && alternatives.length > 0) {
+        alternatives.forEach((alternative) => {
+          alternativesModel.push({
+            id: this.idBuilder.build().id,
+            description: Alternative.getDescription(alternative) as string,
+            questionId
+          })
+        })
+        questionsModel.push({
+          id: questionId,
+          content: Question.getQuestion(question)?.content as string,
+          alternatives: alternativesModel
+        })
+      } else {
+        questionsModel.push({
+          id: questionId,
+          content: Question.getQuestion(question)?.content as string
+        })
+      }
     }
-    await this.addManyQuestionsRepo.addMany(questionsModel)
+    return questionsModel
   }
 }
