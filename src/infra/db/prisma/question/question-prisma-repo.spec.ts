07c7f7a6a0f45559e1db43053c726889947099e3
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 14:00:56 -0300

    test: ensure QuestionPrismaRepo create many Questions with Alternatives on success

--- src/infra/db/prisma/question/question-prisma-repo.spec.ts
+++ src/infra/db/prisma/question/question-prisma-repo.spec.ts
@@ -1,12 +1,29 @@
 import type { PrismaClient } from '@prisma/client'
-import type { QuestionModel } from '@/domain/models/db-models'
+import type { AlternativeModel, QuestionModel } from '@/domain/models/db-models'
 import { PrismockClient } from 'prismock'
 import { PrismaHelper } from '../helpers/prisma-helper'
 import { QuestionPrismaRepo } from './question-prisma-repo'
 
 const makeFakeQuestionsModel = (): QuestionModel[] => ([
-  { id: 'any_id', content: 'any_content' },
-  { id: 'other_id', content: 'other_content' }
+  {
+    id: 'any_id',
+    content: 'any_content',
+    alternatives: makeFakeAlternativesModel()
+  }, {
+    id: 'other_id', content: 'other_content'
+  }
+])
+
+const makeFakeAlternativesModel = (): AlternativeModel[] => ([
+  {
+    id: 'any_alternative_id',
+    description: 'any_description',
+    questionId: 'any_id'
+  }, {
+    id: 'other_alternative_id',
+    description: 'other_description',
+    questionId: 'any_id'
+  }
 ])
 
 let prismock: PrismaClient
@@ -21,16 +38,22 @@ describe('QuestionPrisma Repo', () => {
 
   beforeEach(async () => {
     await prismock.question.deleteMany()
+    await prismock.alternative.deleteMany()
   })
 
   afterAll(async () => {
     await prismock.$disconnect()
   })
 
-  it('Should create many Questions on success', async () => {
+  it('Should create many Questions with Alternatives on success', async () => {
     const sut = new QuestionPrismaRepo()
     await sut.addMany(makeFakeQuestionsModel())
-    const user = await prismock.question.findMany()
-    expect(user).toEqual(makeFakeQuestionsModel())
+    const questions = await prismock.question.findMany()
+    const alternatives = await prismock.alternative.findMany()
+    expect(questions).toEqual([
+      { id: 'any_id', content: 'any_content' },
+      { id: 'other_id', content: 'other_content' }
+    ])
+    expect(alternatives).toEqual(makeFakeAlternativesModel())
   })
 })
