b5a0c2f25cc25a30d33b7ab2104ade17cd7638fa
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 14:00:52 -0300

    feat: ensure QuestionPrismaRepo create many Questions with Alternatives on success

--- src/infra/db/prisma/question/question-prisma-repo.ts
+++ src/infra/db/prisma/question/question-prisma-repo.ts
@@ -1,9 +1,23 @@
-import type { AddManyQuestionsRepo } from '@/interactions/contracts/db'
 import type { QuestionModel } from '@/domain/models/db-models'
+import type { AddManyQuestionsRepo } from '@/interactions/contracts/db'
+import type { Prisma } from '@prisma/client'
 import { PrismaHelper as prisma } from '../helpers/prisma-helper'
 
 export class QuestionPrismaRepo implements AddManyQuestionsRepo {
   async addMany (data: QuestionModel[]): Promise<void> {
-    await (await prisma.getCli()).question.createMany({ data })
+    const questions: QuestionModel[] = data.map((question) => ({
+      id: question.id, content: question.content
+    }))
+    await (await prisma.getCli()).question.createMany({ data: questions })
+    for (const question of data) {
+      if (question.alternatives && question.alternatives.length > 0) {
+        const alternatives: Prisma.AlternativeCreateManyInput[] = question.alternatives.map((alternative) => ({
+          id: alternative.id,
+          description: alternative.description,
+          questionId: alternative.questionId
+        }))
+        await (await prisma.getCli()).alternative.createMany({ data: alternatives })
+      }
+    }
   }
 }
