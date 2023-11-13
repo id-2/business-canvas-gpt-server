9214f6a5f425c1f11026637dec25c40a66beff34
mlucasdev <lucasmsantos819@gmail.com>
2023-11-12 23:29:19 -0300

    test: ensure QuestionPrismaRepo create many Questions on success

---
+++ src/infra/db/prisma/question/question-prisma-repo.spec.ts
@@ -0,0 +1,36 @@
+import type { PrismaClient } from '@prisma/client'
+import type { QuestionModel } from '@/domain/models/db-models'
+import { PrismockClient } from 'prismock'
+import { PrismaHelper } from '../helpers/prisma-helper'
+import { QuestionPrismaRepo } from './question-prisma-repo'
+
+const makeFakeQuestionsModel = (): QuestionModel[] => ([
+  { id: 'any_id', content: 'any_content' },
+  { id: 'other_id', content: 'other_content' }
+])
+
+let prismock: PrismaClient
+
+describe('QuestionPrisma Repo', () => {
+  beforeAll(async () => {
+    prismock = new PrismockClient()
+    jest.spyOn(PrismaHelper, 'getCli').mockReturnValue(
+      Promise.resolve(prismock)
+    )
+  })
+
+  beforeEach(async () => {
+    await prismock.question.deleteMany()
+  })
+
+  afterAll(async () => {
+    await prismock.$disconnect()
+  })
+
+  it('Should create many Questions on success', async () => {
+    const sut = new QuestionPrismaRepo()
+    await sut.add(makeFakeQuestionsModel())
+    const user = await prismock.question.findMany()
+    expect(user).toEqual(makeFakeQuestionsModel())
+  })
+})
