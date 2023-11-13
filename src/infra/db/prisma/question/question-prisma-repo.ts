e11e03f29b97d269015a66f6bfba5671555fe0fd
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 15:51:08 -0300

    feat: ensure QuestionPrismaRepo adreturn all questions with alternatives on success

--- src/infra/db/prisma/question/question-prisma-repo.ts
+++ src/infra/db/prisma/question/question-prisma-repo.ts
@@ -1,8 +1,8 @@
 import type { QuestionModel } from '@/domain/models/db-models'
-import type { AddManyQuestionsRepo } from '@/interactions/contracts/db'
+import type { AddManyQuestionsRepo, FetchAllQuestionsRepo } from '@/interactions/contracts/db'
 import { PrismaHelper as prisma } from '../helpers/prisma-helper'
 
-export class QuestionPrismaRepo implements AddManyQuestionsRepo {
+export class QuestionPrismaRepo implements AddManyQuestionsRepo, FetchAllQuestionsRepo {
   async addMany (data: QuestionModel[]): Promise<void> {
     const prismaCli = await prisma.getCli()
     const questions = data.map(({ id, content }) => ({ id, content }))
@@ -12,5 +12,18 @@ export class QuestionPrismaRepo implements AddManyQuestionsRepo {
         await prismaCli.alternative.createMany({ data: alternatives })
       }
     }
+  }
+
+  async fetchAll (): Promise<null | QuestionModel[]> {
+    const prismaCli = await prisma.getCli()
+    const questions = await prismaCli.question.findMany({
+      include: { Alternative: true }
+    })
+    const questionsMap: QuestionModel[] = questions.map((question) => ({
+      id: question.id,
+      content: question.content,
+      ...(question.Alternative.length > 0 && { alternatives: question.Alternative })
+    }))
+    return questionsMap
   }
 }
