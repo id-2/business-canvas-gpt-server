cf76e0742c51af02b857e542bc65f217ee396c3b
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 19:20:53 -0300

    test: ensure addAndReplyQuestionsSeed() reply all Questions in Cache with Redis

--- src/main/seeds/add-and-reply-questions-seed.test.ts
+++ src/main/seeds/add-and-reply-questions-seed.test.ts
@@ -4,6 +4,7 @@
 
 import type { PrismaClient } from '@prisma/client'
 import type { Redis } from 'ioredis'
+import type { QuestionModel } from '@/domain/models/db-models'
 import { RedisHelper } from '@/infra/cache/redis/helpers/redis-helper'
 import { PrismaHelper } from '@/infra/db/prisma/helpers/prisma-helper'
 import RedisMock from 'ioredis-mock'
@@ -14,21 +15,20 @@ let prisma: PrismaClient
 
 describe('AddManyQuestionsPrisma Seed', () => {
   beforeAll(async () => {
-    await PrismaHelper.connect()
-    prisma = await PrismaHelper.getCli()
     redis = new RedisMock()
     jest.spyOn(RedisHelper, 'getInstance').mockReturnValue(redis)
+    await PrismaHelper.connect()
+    prisma = await PrismaHelper.getCli()
   })
 
-  beforeEach(async () => {
+  afterEach(async () => {
     await prisma.question.deleteMany()
     await prisma.alternative.deleteMany()
-    await redis.flushall()
   })
 
   afterAll(async () => {
-    await prisma.$disconnect()
     redis.disconnect()
+    await prisma.$disconnect()
   })
 
   it('Should add all Questions in DB with Prisma', async () => {
@@ -37,5 +37,12 @@ describe('AddManyQuestionsPrisma Seed', () => {
     const alternatives = await prisma.alternative.findMany()
     expect(questions.length).toBe(3)
     expect(alternatives.length).toBe(2)
+  })
+
+  it('Should reply all Questions in Cache with Redis', async () => {
+    await addAndReplyQuestionsSeed
+    const questionsJson = await redis.get('questions') as string
+    const questions = JSON.parse(questionsJson) as QuestionModel[]
+    expect(questions.length).toBe(3)
   })
 })
