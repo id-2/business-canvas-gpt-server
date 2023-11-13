fb6db41372950c09c88c00d8e65e057e6dd8f71f
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 12:01:15 -0300

    fix: change the alternative field to be a list of AlternativeModel in the QuestionModel

--- src/domain/models/db-models/question.ts
+++ src/domain/models/db-models/question.ts
@@ -3,5 +3,5 @@ import type { AlternativeModel } from './alternative'
 export interface QuestionModel {
   id: string
   content: string
-  alternatives?: AlternativeModel
+  alternatives?: AlternativeModel[]
 }
