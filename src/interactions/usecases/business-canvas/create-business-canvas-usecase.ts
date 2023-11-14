98f62bb547f98a8925365899f4e434a0def11c41
mlucasdev <lucasmsantos819@gmail.com>
2023-11-14 18:02:48 -0300

    feat: ensure CreateBusinessCanvasUseCase call FetchAllQuestionsRepo

--- src/interactions/usecases/business-canvas/create-business-canvas-usecase.ts
+++ src/interactions/usecases/business-canvas/create-business-canvas-usecase.ts
@@ -1,10 +1,15 @@
 import type { AddAnswer, CreateBusinessCanvas, CreateBusinessCanvasDto, CreateBusinessCanvasRes } from '@/domain/contracts'
+import type { FetchAllQuestionsRepo } from '@/interactions/contracts/db'
 import { left, right } from '@/shared/either'
 
 export class CreateBusinessCanvasUseCase implements CreateBusinessCanvas {
-  constructor (private readonly addAnswer: AddAnswer) {}
+  constructor (
+    private readonly fetchAllQuestionsRepo: FetchAllQuestionsRepo,
+    private readonly addAnswer: AddAnswer
+  ) {}
 
   async perform (dto: CreateBusinessCanvasDto): Promise<CreateBusinessCanvasRes> {
+    await this.fetchAllQuestionsRepo.fetchAll()
     const addAnswerResult = await this.addAnswer.perform(dto)
     if (addAnswerResult.isLeft()) {
       return left(addAnswerResult.value)
