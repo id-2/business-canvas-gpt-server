9825cf521b912c75ded446dc15ccb821082e823c
mlucasdev <lucasmsantos819@gmail.com>
2023-11-14 16:22:13 -0300

    feat: ensure CreateBusinessCanvasUseCase return the same error as AddAnswer if it returns an error

--- src/interactions/usecases/business-canvas/create-business-canvas-usecase.ts
+++ src/interactions/usecases/business-canvas/create-business-canvas-usecase.ts
@@ -1,11 +1,14 @@
 import type { AddAnswer, CreateBusinessCanvas, CreateBusinessCanvasDto, CreateBusinessCanvasRes } from '@/domain/contracts'
-import { right } from '@/shared/either'
+import { left, right } from '@/shared/either'
 
 export class CreateBusinessCanvasUseCase implements CreateBusinessCanvas {
   constructor (private readonly addAnswer: AddAnswer) {}
 
   async perform (dto: CreateBusinessCanvasDto): Promise<CreateBusinessCanvasRes> {
-    await this.addAnswer.perform(dto)
+    const addAnswerResult = await this.addAnswer.perform(dto)
+    if (addAnswerResult.isLeft()) {
+      return left(addAnswerResult.value)
+    }
     const object: any = ''
     return right(object)
   }
