a779407d088feceb102fdcef6b7cdb933daa64f5
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 21:04:33 -0300

    feat: ensure AccessControlUseCase return InvalidTokenError if Decrypter returns null

--- src/interactions/usecases/access/access-control/access-control-usecase.ts
+++ src/interactions/usecases/access/access-control/access-control-usecase.ts
@@ -1,13 +1,16 @@
 import type { AccessControl, AccessControlDto, AccessControlRes } from '@/domain/contracts'
 import type { Decrypter } from '@/interactions/contracts/cryptography'
-import { right } from '@/shared/either'
+import { InvalidTokenError } from '@/domain/errors'
+import { left, right } from '@/shared/either'
 
 export class AccessControlUseCase implements AccessControl {
   constructor (private readonly decrypter: Decrypter) {}
 
   async perform (dto: AccessControlDto): Promise<AccessControlRes> {
-    await this.decrypter.decrypt(dto.accessToken)
-
+    const id = await this.decrypter.decrypt(dto.accessToken)
+    if (!id) {
+      return left(new InvalidTokenError())
+    }
     return right({ userId: '' })
   }
 }
