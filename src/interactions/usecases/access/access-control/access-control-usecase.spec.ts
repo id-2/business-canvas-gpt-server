3869b5514dcd433c81f2426a685dbef2b2237038
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 21:02:44 -0300

    test: ensure AccessControlUseCase call Decrypter with correct token

---
+++ src/interactions/usecases/access/access-control/access-control-usecase.spec.ts
@@ -0,0 +1,37 @@
+import type { AccessControlDto } from '@/domain/contracts'
+import type { Decrypter } from '@/interactions/contracts/cryptography'
+import { AccessControlUseCase } from './access-control-usecase'
+
+const makeFakeAccessControlDto = (): AccessControlDto => ({
+  accessToken: 'any_token',
+  role: 'user'
+})
+
+const makeDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt (token: string): Promise<null | string> {
+      return await Promise.resolve('any_id')
+    }
+  }
+  return new DecrypterStub()
+}
+
+interface SutTypes {
+  sut: AccessControlUseCase
+  decrypterStub: Decrypter
+}
+
+const makeSut = (): SutTypes => {
+  const decrypterStub = makeDecrypter()
+  const sut = new AccessControlUseCase(decrypterStub)
+  return { sut, decrypterStub }
+}
+
+describe('AccessControl UseCase', () => {
+  it('Should call Decrypter with correct token', async () => {
+    const { sut, decrypterStub } = makeSut()
+    const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
+    await sut.perform(makeFakeAccessControlDto())
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+})
