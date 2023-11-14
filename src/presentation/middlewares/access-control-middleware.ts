c87522e873976678c2b69958622fc76775f31e4d
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 21:32:37 -0300

    feat: ensure AccessControlMiddleware call AccessControl with correct values

--- src/presentation/middlewares/access-control-middleware.ts
+++ src/presentation/middlewares/access-control-middleware.ts
@@ -1,14 +1,24 @@
 import type { HttpRequest, HttpResponse } from '../http/http'
 import type { Middleware } from '../contracts'
+import type { AccessControl } from '@/domain/contracts'
+import type { Role } from '@/domain/models/db-models'
 import { AccessTokenNotInformedError } from '../errors'
 import { unauthorized } from '../helpers/http/http-helpers'
 
 export class AccessControlMiddleware implements Middleware {
+  constructor (
+    private readonly accessControl: AccessControl,
+    private readonly requiredRole: Role
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token']
     if (!accessToken) {
       return unauthorized(new AccessTokenNotInformedError())
     }
+    await this.accessControl.perform({
+      accessToken, requiredRole: this.requiredRole
+    })
     return { body: '', statusCode: 0 }
   }
 }
