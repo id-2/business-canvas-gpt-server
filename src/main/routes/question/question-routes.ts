e1250c07f7e36617feb73dae90ef3b52cbcae112
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 22:47:15 -0300

    feat: ensure question route return 200 on fetch all questions if the user has permission

--- src/main/routes/question/question-routes.ts
+++ src/main/routes/question/question-routes.ts
@@ -1,7 +1,11 @@
 import type { Router } from 'express'
-import { adaptRoute } from '@/main/adapters'
+import { adaptMiddleware, adaptRoute } from '@/main/adapters'
 import { fetchAllQuestionsControllerFactory } from '@/main/factories/controllers/question/fetch-all-questions-controller-factory'
+import { accessControlMiddlewareFactory } from '@/main/factories/middlewares/access-control-middleware-factory'
 
 export default async (router: Router): Promise<void> => {
-  router.get('/question', adaptRoute(fetchAllQuestionsControllerFactory()))
+  router.get('/question',
+    adaptMiddleware(accessControlMiddlewareFactory('user')),
+    adaptRoute(fetchAllQuestionsControllerFactory())
+  )
 }
