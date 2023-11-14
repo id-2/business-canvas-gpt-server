383ee1c97dad0a67cf95ed057c9431e5a438ba2d
mlucasdev <lucasmsantos819@gmail.com>
2023-11-13 21:32:42 -0300

    test: ensure AccessControlMiddleware call AccessControl with correct values

--- src/presentation/middlewares/access-control-middleware.spec.ts
+++ src/presentation/middlewares/access-control-middleware.spec.ts
@@ -1,14 +1,37 @@
+import type { AccessControl, AccessControlDto, AccessControlRes } from '@/domain/contracts'
+import type { HttpRequest } from '../http/http'
 import { AccessTokenNotInformedError } from '../errors'
 import { unauthorized } from '../helpers/http/http-helpers'
 import { AccessControlMiddleware } from './access-control-middleware'
+import { right } from '@/shared/either'
+
+const makeFakeRequest = (): HttpRequest => ({
+  headers: { 'x-access-token': 'any_token' }
+})
+
+const makeFakeAccessControlDto = (): AccessControlDto => ({
+  accessToken: 'any_token',
+  requiredRole: 'admin'
+})
+
+const makeAccessControl = (): AccessControl => {
+  class AccessControlStub implements AccessControl {
+    async perform (data: AccessControlDto): Promise<AccessControlRes> {
+      return await Promise.resolve(right({ userId: 'any_id' }))
+    }
+  }
+  return new AccessControlStub()
+}
 
 interface SutTypes {
   sut: AccessControlMiddleware
+  accessControlStub: AccessControl
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AccessControlMiddleware()
-  return { sut }
+  const accessControlStub = makeAccessControl()
+  const sut = new AccessControlMiddleware(accessControlStub, 'admin')
+  return { sut, accessControlStub }
 }
 
 describe('AccessControl Middleware', () => {
@@ -16,5 +39,12 @@ describe('AccessControl Middleware', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(unauthorized(new AccessTokenNotInformedError()))
+  })
+
+  it('Should call AccessControl with correct values', async () => {
+    const { sut, accessControlStub } = makeSut()
+    const performSpy = jest.spyOn(accessControlStub, 'perform')
+    await sut.handle(makeFakeRequest())
+    expect(performSpy).toHaveBeenCalledWith(makeFakeAccessControlDto())
   })
 })
