2d8b3d9728aee4454ac5904c712a11db5cfa9d4c
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 10:26:36 -0300

    feat: ensure AddRandomUserUseCase call IdBuilder

--- src/interactions/usecases/user/add-random-user/add-random-user-usecase.ts
+++ src/interactions/usecases/user/add-random-user/add-random-user-usecase.ts
@@ -1,13 +1,18 @@
 import type { AddRandomUser } from '@/domain/contracts'
 import type { Hasher } from '../../../contracts/cryptography'
 import type { IdModel } from '@/domain/models/output-models'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
 import env from '@/main/configs/env'
 
 export class AddRandomUserUseCase implements AddRandomUser {
-  constructor (private readonly hasher: Hasher) {}
+  constructor (
+    private readonly hasher: Hasher,
+    private readonly idBuilder: IdBuilder
+  ) {}
 
   async perform (): Promise<IdModel> {
     await this.hasher.hashing(env.randomUserPassword)
+    this.idBuilder.build()
     return { id: '' }
   }
 }
