723ec36ee45dc15e507694f9ef61294e8302959e
mlucasdev <lucasmsantos819@gmail.com>
2023-11-14 21:52:51 -0300

    feat: ensure CreateBusinessCanvasUseCase call Answer Entity with correct values

--- src/interactions/usecases/business-canvas/create-business-canvas-usecase.ts
+++ src/interactions/usecases/business-canvas/create-business-canvas-usecase.ts
@@ -2,6 +2,7 @@ import type { AddAnswer, CreateBusinessCanvas, CreateBusinessCanvasDto, CreateBu
 import type { FetchAllQuestionsRepo } from '@/interactions/contracts/db'
 import { QuestionsNotFoundError } from '@/domain/errors'
 import { left, right } from '@/shared/either'
+import { Answer } from '@/domain/entities/answer/answer'
 
 export class CreateBusinessCanvasUseCase implements CreateBusinessCanvas {
   constructor (
@@ -14,6 +15,7 @@ export class CreateBusinessCanvasUseCase implements CreateBusinessCanvas {
     if (questions.length === 0) {
       throw new QuestionsNotFoundError()
     }
+    Answer.createMany({ userAnswers: dto.answers, questions })
     const addAnswerResult = await this.addAnswer.perform(dto)
     if (addAnswerResult.isLeft()) {
       return left(addAnswerResult.value)
