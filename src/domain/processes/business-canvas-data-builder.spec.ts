59d6a32513c4e08b9c2cd28ea8bb48f17b64da6a
mlucasdev <lucasmsantos819@gmail.com>
2023-11-14 23:53:55 -0300

    test: ensure BusinessCanvasDataBuilder return an BusinessCanvasDataBuilderRes formatted no location

--- src/domain/processes/business-canvas-data-builder.spec.ts
+++ src/domain/processes/business-canvas-data-builder.spec.ts
@@ -3,38 +3,52 @@ import type { QuestionModel } from '../models/db-models'
 import { BusinessCanvasDataBuilder as sut } from './business-canvas-data-builder'
 
 const makeFakeQuestionsModel = (): QuestionModel[] => ([{
-  id: 'any_question_id',
+  id: 'type_question_id',
   content: 'Qual o tipo do seu negócio?',
   alternatives: [{
-    id: 'any_alternative_id',
-    description: 'Presencial',
-    questionId: 'any_question_id'
+    id: 'in_person_alternative_id',
+    description: 'in_person',
+    questionId: 'type_question_id'
   }, {
-    id: 'other_alternative_id',
-    description: 'Online',
-    questionId: 'any_question_id'
+    id: 'online_alternative_id',
+    description: 'online',
+    questionId: 'type_question_id'
   }]
 }, {
-  id: 'other_question_id', content: 'Qual a localização ou público para o qual deseja trabalhar (Cidade, estado ou país)?'
+  id: 'location_question_id', content: 'Qual a localização ou público para o qual deseja trabalhar (Cidade, estado ou país)?'
 }, {
-  id: 'another_question_id', content: 'Descreva seu negócio:'
+  id: 'description_question_id', content: 'Descreva seu negócio:'
 }])
 
 const makeFakeUserAnswers = (): UserAnswer[] => ([
-  { questionId: 'any_question_id', alternativeId: 'any_alternative_id' },
-  { questionId: 'other_question_id', answer: 'any_answer' },
-  { questionId: 'another_question_id', answer: 'other_answer' }
+  { questionId: 'type_question_id', alternativeId: 'in_person_alternative_id' },
+  { questionId: 'location_question_id', answer: 'location_answer' },
+  { questionId: 'description_question_id', answer: 'description_answer' }
 ])
 
 describe('BusinessCanvasDataBuilder', () => {
-  it('Should return an BusinessCanvasDataBuilderRes formated', async () => {
+  it('Should return an BusinessCanvasDataBuilderRes formatted ', async () => {
     const result = sut.execute({
       userAnswers: makeFakeUserAnswers(), questions: makeFakeQuestionsModel()
     })
     expect(result).toEqual({
-      typeOfBusiness: 'Presencial',
-      businessDescription: 'other_answer',
-      locationOrTargetAudience: 'any_answer'
+      typeOfBusiness: 'in_person',
+      businessDescription: 'description_answer',
+      locationOrTargetAudience: 'location_answer'
+    })
+  })
+
+  it('Should return an BusinessCanvasDataBuilderRes formatted no location', async () => {
+    const result = sut.execute({
+      userAnswers: [
+        { questionId: 'type_question_id', alternativeId: 'online_alternative_id' },
+        { questionId: 'description_question_id', answer: 'description_answer' }
+      ],
+      questions: makeFakeQuestionsModel()
+    })
+    expect(result).toEqual({
+      typeOfBusiness: 'online',
+      businessDescription: 'description_answer'
     })
   })
 })
