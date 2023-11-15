97ba3447b4e4f04474ed0ba2fbbab0cc9ab41a7a
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 10:26:42 -0300

    test: ensure AddRandomUserUseCase call IdBuilder

--- src/interactions/usecases/user/add-random-user/add-random-user-usecase.spec.ts
+++ src/interactions/usecases/user/add-random-user/add-random-user-usecase.spec.ts
@@ -1,5 +1,6 @@
-import type { HashedModel } from '@/domain/models/output-models'
-import type { Hasher } from '../../../contracts/cryptography'
+import type { HashedModel, IdModel } from '@/domain/models/output-models'
+import type { Hasher } from '@/interactions/contracts/cryptography'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
 import { AddRandomUserUseCase } from './add-random-user-usecase'
 
 const makeHasher = (): Hasher => {
@@ -11,15 +12,26 @@ const makeHasher = (): Hasher => {
   return new HasherStub()
 }
 
+const makeIdBuilder = (): IdBuilder => {
+  class IdBuilderStub implements IdBuilder {
+    build (): IdModel {
+      return { id: 'any_id' }
+    }
+  }
+  return new IdBuilderStub()
+}
+
 interface SutTypes {
   sut: AddRandomUserUseCase
   hasherStub: Hasher
+  idBuilderStub: IdBuilder
 }
 
 const makeSut = (): SutTypes => {
   const hasherStub = makeHasher()
-  const sut = new AddRandomUserUseCase(hasherStub)
-  return { sut, hasherStub }
+  const idBuilderStub = makeIdBuilder()
+  const sut = new AddRandomUserUseCase(hasherStub, idBuilderStub)
+  return { sut, hasherStub, idBuilderStub }
 }
 
 describe('AddRandomUser UseCase', () => {
@@ -37,5 +49,12 @@ describe('AddRandomUser UseCase', () => {
     )
     const promise = sut.perform()
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call IdBuilder', async () => {
+    const { sut, idBuilderStub } = makeSut()
+    const buildSpy = jest.spyOn(idBuilderStub, 'build')
+    await sut.perform()
+    expect(buildSpy).toHaveBeenCalled()
   })
 })
