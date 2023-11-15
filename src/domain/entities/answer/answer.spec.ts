7fb6dfccdfe6852e21371189bf2f72353ed1c27d
mlucasdev <lucasmsantos819@gmail.com>
2023-11-14 21:14:50 -0300

    test: ensure Answer createMany method call create method with all user answers

--- src/domain/entities/answer/answer.spec.ts
+++ src/domain/entities/answer/answer.spec.ts
@@ -1,6 +1,7 @@
-import type { QuestionModel } from '@/domain/models/db-models'
-import { Answer as sut } from './answer'
 import { AlternativeIsNotAllowedError, AnswerAndAlternativeNotProvidedError, AnswerIsNotAllowedError, InvalidAlternativeIdError, InvalidAnswerError, InvalidQuestionIdError } from './errors'
+import type { QuestionModel } from '@/domain/models/db-models'
+import type { UserAnswer } from './answer-dto'
+import { Answer, Answer as sut } from './answer'
 import { MixedAnswerError } from './errors/mixed-answer-error'
 
 const makeFakeQuestionsModel = (): QuestionModel[] => ([{
@@ -19,99 +20,122 @@ const makeFakeQuestionsModel = (): QuestionModel[] => ([{
   id: 'other_question_id', content: 'other_content'
 }])
 
+const makeFakeUserAnswers = (): UserAnswer[] => ([
+  { questionId: 'any_question_id', alternativeId: 'other_alternative_id' },
+  { questionId: 'other_question_id', answer: 'any_answer' }
+])
+
 describe('Answer Entity', () => {
-  it('Should return AnswerAndAlternativeNotProvidedError if answer and a alternativeId not provided', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'any_question_id' },
-      questions: makeFakeQuestionsModel()
+  describe('create()', () => {
+    it('Should return AnswerAndAlternativeNotProvidedError if answer and a alternativeId not provided', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'any_question_id' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new AnswerAndAlternativeNotProvidedError())
     })
-    expect(result.value).toEqual(new AnswerAndAlternativeNotProvidedError())
+
+    it('Should return MixedAnswerError if answer and a alternativeId is provided', () => {
+      const result = sut.create({
+        userAnswer: {
+          questionId: 'any_question_id',
+          alternativeId: 'invalid_alternative_id',
+          answer: 'invalid_answer'
+        },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new MixedAnswerError())
+    })
+
+    it('Should return InvalidQuestionIdError if user answer questionId is invalid', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'invalid_question_id', answer: 'any_answer' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new InvalidQuestionIdError('invalid_question_id'))
+    })
+
+    it('Should return AnswerIsNotAllowedError if the answer field is sent for a question that does not require it', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'any_question_id', answer: 'any_answer' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new AnswerIsNotAllowedError())
+    })
+
+    it('Should return AlternativeIsNotAllowedError if the alternativeId field is sent for a question that does not require it', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'other_question_id', alternativeId: 'any_alternative_id' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new AlternativeIsNotAllowedError())
+    })
+
+    it('Should return InvalidAlternativeIdError if user answer alternativeId is invalid', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'any_question_id', alternativeId: 'invalid_alternative_id' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new InvalidAlternativeIdError('invalid_alternative_id'))
+    })
+
+    it('Should return InvalidAnswerError if answer is less than 3 characters', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'other_question_id', answer: 'ab' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new InvalidAnswerError('ab'))
+    })
+
+    it('Should return InvalidAnswerError if answer greater than 750 characters', () => {
+      const answer = 'a'.repeat(751)
+      const result = sut.create({
+        userAnswer: { questionId: 'other_question_id', answer },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual(new InvalidAnswerError(answer))
+    })
+
+    it('Should return a new Answer with answer alternativeId if validations is a success', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'any_question_id', alternativeId: 'any_alternative_id' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual({
+        answer: {
+          questionId: 'any_question_id',
+          alternativeId: 'any_alternative_id'
+        }
+      })
+    })
+
+    it('Should return a new Answer with answer field if validations is a success', () => {
+      const result = sut.create({
+        userAnswer: { questionId: 'other_question_id', answer: 'any_answer' },
+        questions: makeFakeQuestionsModel()
+      })
+      expect(result.value).toEqual({
+        answer: {
+          questionId: 'other_question_id',
+          answer: 'any_answer'
+        }
+      })
+    })
   })
 
-  it('Should return MixedAnswerError if answer and a alternativeId is provided', () => {
-    const result = sut.create({
-      userAnswer: {
-        questionId: 'any_question_id',
-        alternativeId: 'invalid_alternative_id',
-        answer: 'invalid_answer'
-      },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new MixedAnswerError())
-  })
-
-  it('Should return InvalidQuestionIdError if user answer questionId is invalid', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'invalid_question_id', answer: 'any_answer' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new InvalidQuestionIdError('invalid_question_id'))
-  })
-
-  it('Should return AnswerIsNotAllowedError if the answer field is sent for a question that does not require it', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'any_question_id', answer: 'any_answer' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new AnswerIsNotAllowedError())
-  })
-
-  it('Should return AlternativeIsNotAllowedError if the alternativeId field is sent for a question that does not require it', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'other_question_id', alternativeId: 'any_alternative_id' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new AlternativeIsNotAllowedError())
-  })
-
-  it('Should return InvalidAlternativeIdError if user answer alternativeId is invalid', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'any_question_id', alternativeId: 'invalid_alternative_id' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new InvalidAlternativeIdError('invalid_alternative_id'))
-  })
-
-  it('Should return InvalidAnswerError if answer is less than 3 characters', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'other_question_id', answer: 'ab' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new InvalidAnswerError('ab'))
-  })
-
-  it('Should return InvalidAnswerError if answer greater than 750 characters', () => {
-    const answer = 'a'.repeat(751)
-    const result = sut.create({
-      userAnswer: { questionId: 'other_question_id', answer },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual(new InvalidAnswerError(answer))
-  })
-
-  it('Should return a new Answer with answer alternativeId if validations is a success', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'any_question_id', alternativeId: 'any_alternative_id' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual({
-      answer: {
-        questionId: 'any_question_id',
-        alternativeId: 'any_alternative_id'
-      }
-    })
-  })
-
-  it('Should return a new Answer with answer field if validations is a success', () => {
-    const result = sut.create({
-      userAnswer: { questionId: 'other_question_id', answer: 'any_answer' },
-      questions: makeFakeQuestionsModel()
-    })
-    expect(result.value).toEqual({
-      answer: {
-        questionId: 'other_question_id',
-        answer: 'any_answer'
-      }
+  describe('createMany()', () => {
+    it('Should call create method with all user answers', () => {
+      const createSpy = jest.spyOn(Answer, 'create')
+      Answer.createMany({
+        userAnswers: makeFakeUserAnswers(),
+        questions: makeFakeQuestionsModel()
+      })
+      expect(createSpy).toHaveBeenCalledWith({
+        userAnswer: makeFakeUserAnswers()[0], questions: makeFakeQuestionsModel()
+      })
+      expect(createSpy).toHaveBeenCalledWith({
+        userAnswer: makeFakeUserAnswers()[1], questions: makeFakeQuestionsModel()
+      })
     })
   })
 })
