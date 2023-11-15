3b72292feb8db517dd217f6e6a2890f6f8e49df3
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 10:40:59 -0300

    feat: ensure AddRandomUserUseCase call AddUserRepo with correct values

--- src/interactions/usecases/user/add-random-user/add-random-user-usecase.ts
+++ src/interactions/usecases/user/add-random-user/add-random-user-usecase.ts
@@ -2,17 +2,26 @@ import type { AddRandomUser } from '@/domain/contracts'
 import type { Hasher } from '../../../contracts/cryptography'
 import type { IdModel } from '@/domain/models/output-models'
 import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import type { AddUserRepo } from '@/interactions/contracts/db'
 import env from '@/main/configs/env'
 
 export class AddRandomUserUseCase implements AddRandomUser {
   constructor (
     private readonly hasher: Hasher,
-    private readonly idBuilder: IdBuilder
+    private readonly idBuilder: IdBuilder,
+    private readonly addUserRepo: AddUserRepo
   ) {}
 
   async perform (): Promise<IdModel> {
-    await this.hasher.hashing(env.randomUserPassword)
-    this.idBuilder.build()
+    const { hash: password } = await this.hasher.hashing(env.randomUserPassword)
+    const { id } = this.idBuilder.build()
+    const { id: random } = this.idBuilder.build()
+    const email = `${random}@convidado.com`
+    const date = new Date()
+    const createdAt = date; const updatedAt = date
+    await this.addUserRepo.add({
+      id, password, email, name: 'Convidado', createdAt, updatedAt, role: 'user'
+    })
     return { id: '' }
   }
 }
