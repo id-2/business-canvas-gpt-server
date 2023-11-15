87b29a21840db0ce60253e335d904c4f3c61a5ca
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 10:41:05 -0300

    test: ensure AddRandomUserUseCase call AddUserRepo with correct values

--- src/interactions/usecases/user/add-random-user/add-random-user-usecase.spec.ts
+++ src/interactions/usecases/user/add-random-user/add-random-user-usecase.spec.ts
@@ -1,7 +1,20 @@
 import type { HashedModel, IdModel } from '@/domain/models/output-models'
 import type { Hasher } from '@/interactions/contracts/cryptography'
 import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import type { UserModel } from '@/domain/models/db-models'
+import type { AddUserRepo } from '@/interactions/contracts/db'
 import { AddRandomUserUseCase } from './add-random-user-usecase'
+import MockDate from 'mockdate'
+
+const makeFakeUserModel = (): UserModel => ({
+  id: 'any_id_1',
+  name: 'Convidado',
+  email: 'any_id_2@convidado.com',
+  password: 'hashed_password',
+  role: 'user',
+  createdAt: new Date(),
+  updatedAt: new Date()
+})
 
 const makeHasher = (): Hasher => {
   class HasherStub implements Hasher {
@@ -13,28 +26,49 @@ const makeHasher = (): Hasher => {
 }
 
 const makeIdBuilder = (): IdBuilder => {
-  class IdBuilderStub implements IdBuilder {
+  class IdBuilderSpy implements IdBuilder {
+    private callsCount = 0
     build (): IdModel {
-      return { id: 'any_id' }
+      this.callsCount++
+      return { id: `any_id_${this.callsCount}` }
     }
   }
-  return new IdBuilderStub()
+  return new IdBuilderSpy()
+}
+
+const makeAddUserRepo = (): AddUserRepo => {
+  class AddUserRepoStub implements AddUserRepo {
+    async add (data: UserModel): Promise<void> {
+      await Promise.resolve()
+    }
+  }
+  return new AddUserRepoStub()
 }
 
 interface SutTypes {
   sut: AddRandomUserUseCase
   hasherStub: Hasher
   idBuilderStub: IdBuilder
+  addUserRepoStub: AddUserRepo
 }
 
 const makeSut = (): SutTypes => {
   const hasherStub = makeHasher()
   const idBuilderStub = makeIdBuilder()
-  const sut = new AddRandomUserUseCase(hasherStub, idBuilderStub)
-  return { sut, hasherStub, idBuilderStub }
+  const addUserRepoStub = makeAddUserRepo()
+  const sut = new AddRandomUserUseCase(hasherStub, idBuilderStub, addUserRepoStub)
+  return { sut, hasherStub, idBuilderStub, addUserRepoStub }
 }
 
 describe('AddRandomUser UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   it('Should call Hasher with correct password', async () => {
     const { sut, hasherStub } = makeSut()
     const hashingSpy = jest.spyOn(hasherStub, 'hashing')
@@ -65,5 +99,12 @@ describe('AddRandomUser UseCase', () => {
     })
     const promise = sut.perform()
     await expect(promise).rejects.toThrow()
+  })
+
+  it('Should call AddUserRepo with correct values', async () => {
+    const { sut, addUserRepoStub } = makeSut()
+    const addSpy = jest.spyOn(addUserRepoStub, 'add')
+    await sut.perform()
+    expect(addSpy).toHaveBeenCalledWith(makeFakeUserModel())
   })
 })
