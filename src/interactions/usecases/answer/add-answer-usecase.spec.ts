09e955daa1e019e6fe1c47af419dd8c689af8466
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 14:51:12 -0300

    test: ensure AddAnswerUseCase call IdBuilder

---
+++ src/interactions/usecases/answer/add-answer-usecase.spec.ts
@@ -0,0 +1,42 @@
+import type { IdModel } from '@/domain/models/output-models'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
+import type { AddAnswerDto } from '@/domain/contracts'
+import { AddAnswerUseCase } from './add-answer-usecase'
+
+const makeFakeAddAnswerDto = (): AddAnswerDto => ({
+  userId: 'any_user_id',
+  answers: [
+    { questionId: 'type_question_id', alternativeId: 'in_person_alternative_id' },
+    { questionId: 'location_question_id', answer: 'location_answer' },
+    { questionId: 'description_question_id', answer: 'description_answer' }
+  ]
+})
+
+const makeIdBuilder = (): IdBuilder => {
+  class IdBuilderStub implements IdBuilder {
+    build (): IdModel {
+      return { id: 'any_id' }
+    }
+  }
+  return new IdBuilderStub()
+}
+
+interface SutTypes {
+  sut: AddAnswerUseCase
+  idBuilderStub: IdBuilder
+}
+
+const makeSut = (): SutTypes => {
+  const idBuilderStub = makeIdBuilder()
+  const sut = new AddAnswerUseCase(idBuilderStub)
+  return { sut, idBuilderStub }
+}
+
+describe('AddAnswer UseCase', () => {
+  it('Should call IdBuilder', async () => {
+    const { sut, idBuilderStub } = makeSut()
+    const buildSpy = jest.spyOn(idBuilderStub, 'build')
+    await sut.perform(makeFakeAddAnswerDto())
+    expect(buildSpy).toHaveBeenCalled()
+  })
+})
