a6232f53b3d1df4b6c5c5892d478ba44f0dd06c9
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 12:44:10 -0300

    test: ensure GenerateInputToCreateBusinessCanvas not call TemplateForInputOnlineBusiness  if locationOrTargetAudience is provided

--- src/domain/processes/generate-input-to-create-business-canvas.spec.ts
+++ src/domain/processes/generate-input-to-create-business-canvas.spec.ts
@@ -1,6 +1,18 @@
 import type { GenerateInputToCreateBusinessCanvasDto } from './generate-input-to-create-business-canvas'
 import { GenerateInputToCreateBusinessCanvas as sut } from './generate-input-to-create-business-canvas'
-import { TemplateForInputInPersonBusiness } from './input-templates'
+import { TemplateForInputInPersonBusiness, TemplateForInputOnlineBusiness } from './input-templates'
+
+jest.mock('@/domain/processes/input-templates/input-in-person-business/template-for-input-in-person-business', () => ({
+  TemplateForInputInPersonBusiness: {
+    create: jest.fn(() => ('any_input'))
+  }
+}))
+
+jest.mock('@/domain/processes/input-templates/input-online-business/template-for-input-online-business', () => ({
+  TemplateForInputOnlineBusiness: {
+    create: jest.fn(() => ('other_input'))
+  }
+}))
 
 const makeFakeGenerateInputDto = (): GenerateInputToCreateBusinessCanvasDto => ({
   typeOfBusiness: 'in_person',
@@ -9,9 +21,33 @@ const makeFakeGenerateInputDto = (): GenerateInputToCreateBusinessCanvasDto => (
 })
 
 describe('GenerateInputToCreateBusinessCanvas', () => {
-  it('Should call TemplateForInputInPersonBusiness', async () => {
+  it('Should call TemplateForInputInPersonBusiness if locationOrTargetAudience is provided', async () => {
     const createSpy = jest.spyOn(TemplateForInputInPersonBusiness, 'create')
     sut.execute(makeFakeGenerateInputDto())
     expect(createSpy).toHaveBeenCalled()
+  })
+
+  it('Should not call TemplateForInputInPersonBusiness if locationOrTargetAudience not provided', async () => {
+    const createSpy = jest.spyOn(TemplateForInputInPersonBusiness, 'create')
+    sut.execute({
+      typeOfBusiness: 'in_person',
+      businessDescription: 'description_answer'
+    })
+    expect(createSpy).not.toHaveBeenCalled()
+  })
+
+  it('Should call TemplateForInputOnlineBusiness if locationOrTargetAudience not provided', async () => {
+    const createSpy = jest.spyOn(TemplateForInputOnlineBusiness, 'create')
+    sut.execute({
+      typeOfBusiness: 'in_person',
+      businessDescription: 'description_answer'
+    })
+    expect(createSpy).toHaveBeenCalled()
+  })
+
+  it('Should not call TemplateForInputOnlineBusiness if locationOrTargetAudience is provided', async () => {
+    const createSpy = jest.spyOn(TemplateForInputOnlineBusiness, 'create')
+    sut.execute(makeFakeGenerateInputDto())
+    expect(createSpy).not.toHaveBeenCalled()
   })
 })
