1fa34dbea58cf0f3df0a6f34f81e27cc7efbc140
mlucasdev <lucasmsantos819@gmail.com>
2023-11-14 23:53:48 -0300

    feat: ensure BusinessCanvasDataBuilder return an BusinessCanvasDataBuilderRes formatted no location

--- src/domain/processes/business-canvas-data-builder.ts
+++ src/domain/processes/business-canvas-data-builder.ts
@@ -12,25 +12,29 @@ export type BusinessCanvasDataBuilderRes = GenerateInputToCreateBusinessCanvasDt
 export class BusinessCanvasDataBuilder {
   static execute (dto: BusinessCanvasDataBuilderDto): BusinessCanvasDataBuilderRes {
     const { userAnswers, questions } = dto
-    const object: BusinessCanvasDataBuilderRes = {
-      businessDescription: '', typeOfBusiness: '', locationOrTargetAudience: ''
-    }
+    const object = { businessDescription: '', typeOfBusiness: '', locationOrTargetAudience: '' }
     for (const userAnswer of userAnswers) {
       const question = questions.find(question => question.id === userAnswer.questionId) as QuestionModel
       if (this.regexLocation().test(question.content)) {
         object.locationOrTargetAudience = userAnswer.answer as string
-      } else if (this.regexBusinessType().test(question.content)) {
+      }
+      if (this.regexBusinessType().test(question.content)) {
         const alternative = question.alternatives?.find(alternative => alternative.id === userAnswer.alternativeId)
         object.typeOfBusiness = alternative?.description as string
-      } else if (this.regexDescription().test(question.content)) {
+      }
+      if (this.regexDescription().test(question.content)) {
         object.businessDescription = userAnswer.answer as string
       }
     }
-    return object
+    return {
+      businessDescription: object.businessDescription,
+      typeOfBusiness: object.typeOfBusiness,
+      ...(object.locationOrTargetAudience?.length > 0 && { locationOrTargetAudience: object.locationOrTargetAudience })
+    }
   }
 
   private static regexLocation (): RegExp {
-    const keysWords = ['localização', 'público', 'cidade', 'estado', 'trabalhar']
+    const keysWords = ['localização', 'público', 'local', 'cidade', 'estado', 'trabalhar']
     return new RegExp(keysWords.join('|'), 'gi')
   }
 
