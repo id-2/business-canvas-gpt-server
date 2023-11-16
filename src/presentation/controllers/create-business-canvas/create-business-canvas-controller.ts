c88ec8732d5b61cf4333a23e21aa825865bbac18
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 22:11:20 -0300

    feat: ensure CreateBusinessCanvasController return 400 if Validation fails

--- src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
+++ src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
@@ -1,11 +1,15 @@
 import type { Controller, Validation } from '@/presentation/contracts'
 import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
+import { badRequest } from '@/presentation/helpers/http/http-helpers'
 
 export class CreateBusinessCanvasController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const validationResult = this.validation.validate(httpRequest.body)
+    if (validationResult.isLeft()) {
+      return badRequest(validationResult.value)
+    }
     return { statusCode: 0, body: '' }
   }
 }
