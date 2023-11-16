854cc1c824f9c43ec0a4ec32f9cc877de456c4c7
mlucasdev <lucasmsantos819@gmail.com>
2023-11-16 14:40:21 -0300

    refactor: move AddUserUseCase to add-user folder

---
+++ src/interactions/usecases/user/index.ts
@@ -0,0 +1,2 @@
+export * from './add-random-user/add-random-user-usecase'
+export * from './add-user/add-user-usecase'
--- src/main/factories/usecases/user/add-user-usecase-factory.ts
+++ src/main/factories/usecases/user/add-user-usecase-factory.ts
@@ -1,5 +1,5 @@
 import type { AddUser } from '@/domain/contracts'
-import { AddUserUseCase } from '@/interactions/usecases/user/add-user-usecase'
+import { AddUserUseCase } from '@/interactions/usecases/user'
 import { bcryptAdapterFactory } from '@/main/factories/infra/cryptography/bcrypt-adapter-factory'
 import { userPrismaRepoFactory } from '@/main/factories/infra/db/prisma/user-prisma-repo-factory'
 import { uuidAdapterFactory } from '@/main/factories/infra/id/uuid-adapter-factory'
