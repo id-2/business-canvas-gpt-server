e59b196a754bd191d46de62437bce2b271753a1c
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 22:23:28 -0300

    feat: ensure CreateBusinessCanvasController call CreateBusinessCanvas with correct values

--- src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
+++ src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
@@ -1,9 +1,13 @@
 import type { Controller, Validation } from '@/presentation/contracts'
 import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
+import type { CreateBusinessCanvas } from '@/domain/contracts'
 import { badRequest, serverError } from '@/presentation/helpers/http/http-helpers'
 
 export class CreateBusinessCanvasController implements Controller {
-  constructor (private readonly validation: Validation) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly createBusinessCanvas: CreateBusinessCanvas
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -11,6 +15,10 @@ export class CreateBusinessCanvasController implements Controller {
       if (validationResult.isLeft()) {
         return badRequest(validationResult.value)
       }
+      const { userId } = httpRequest.headers
+      await this.createBusinessCanvas.perform({
+        userId, answers: httpRequest.body
+      })
       return { statusCode: 0, body: '' }
     } catch (error: any) {
       return serverError(error)
