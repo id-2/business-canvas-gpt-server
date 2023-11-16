fbc4ea11b78c1d52a94e00b6d5694ffeb9945bff
mlucasdev <lucasmsantos819@gmail.com>
2023-11-16 13:17:11 -0300

    feat: ensure AddAllComponentsUseCase call IdBuilder nine times

--- src/interactions/usecases/component/add-all-componetns-usecase.ts
+++ src/interactions/usecases/component/add-all-componetns-usecase.ts
@@ -1,19 +1,21 @@
 import type { AddAllComponents } from '@/domain/contracts'
 import type { AddAllComponentsRepo } from '@/interactions/contracts/db'
 import type { ComponentModel } from '@/domain/models/db-models'
+import type { IdBuilder } from '@/interactions/contracts/id/id-builder'
 import { Component, type ComponentName } from '@/domain/entities/component'
 
 export class AddAllComponentsUseCase implements AddAllComponents {
-  constructor (private readonly addAllComponentsRepo: AddAllComponentsRepo) {}
+  constructor (
+    private readonly idBuilder: IdBuilder,
+    private readonly addAllComponentsRepo: AddAllComponentsRepo
+  ) {}
 
   async perform (): Promise<void> {
     const components = Component.createMany()
     const componentModels: ComponentModel[] = []
-    let count = 0
     for (const component of components) {
       const name = Component.getComponent(component) as ComponentName
-      count++
-      const id = `any_id_${count}`
+      const { id } = this.idBuilder.build()
       componentModels.push({ id, name })
     }
     await this.addAllComponentsRepo.addAll(componentModels)
