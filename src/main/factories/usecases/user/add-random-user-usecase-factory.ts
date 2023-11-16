6f9c5fbd045c4aa833c99572280a0419e90db18b
mlucasdev <lucasmsantos819@gmail.com>
2023-11-16 14:42:54 -0300

    feat: create AddRandomUserUseCase Factory

---
+++ src/main/factories/usecases/user/add-random-user-usecase-factory.ts
@@ -0,0 +1,13 @@
+import type { AddRandomUser } from '@/domain/contracts'
+import { AddRandomUserUseCase } from '@/interactions/usecases/user'
+import { bcryptAdapterFactory } from '@/main/factories/infra/cryptography/bcrypt-adapter-factory'
+import { userPrismaRepoFactory } from '@/main/factories/infra/db/prisma/user-prisma-repo-factory'
+import { uuidAdapterFactory } from '@/main/factories/infra/id/uuid-adapter-factory'
+
+export const addUserUseCaseFactory = (): AddRandomUser => {
+  return new AddRandomUserUseCase(
+    bcryptAdapterFactory(),
+    uuidAdapterFactory(),
+    userPrismaRepoFactory()
+  )
+}
