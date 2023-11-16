12d1f0a0d76830ad337eea3455d5dd095b3cfc1b
mlucasdev <lucasmsantos819@gmail.com>
2023-11-15 22:13:05 -0300

    feat: ensure CreateBusinessCanvasController return 500 if Validation throws

--- src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
+++ src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
@@ -1,15 +1,19 @@
 import type { Controller, Validation } from '@/presentation/contracts'
 import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
-import { badRequest } from '@/presentation/helpers/http/http-helpers'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helpers'
 
 export class CreateBusinessCanvasController implements Controller {
   constructor (private readonly validation: Validation) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const validationResult = this.validation.validate(httpRequest.body)
-    if (validationResult.isLeft()) {
-      return badRequest(validationResult.value)
+    try {
+      const validationResult = this.validation.validate(httpRequest.body)
+      if (validationResult.isLeft()) {
+        return badRequest(validationResult.value)
+      }
+      return { statusCode: 0, body: '' }
+    } catch (error: any) {
+      return serverError(error)
     }
-    return { statusCode: 0, body: '' }
   }
 }
