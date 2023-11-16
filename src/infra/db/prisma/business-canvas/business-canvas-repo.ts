cb2fe189bf29f92a21e858b28ac13a2404323e53
mlucasdev <lucasmsantos819@gmail.com>
2023-11-16 17:03:34 -0300

    feat: ensure that BusinessCanvasPrismaRepo complete completes all transactions or returns to the database initial state in case of an error

--- src/infra/db/prisma/business-canvas/business-canvas-repo.ts
+++ src/infra/db/prisma/business-canvas/business-canvas-repo.ts
@@ -1,19 +1,21 @@
 import type { BusinessCanvasModel } from '@/domain/models/db-models'
 import type { AddBusinessCanvasRepo } from '@/interactions/contracts/db'
-import type { ComponentName } from '@/domain/entities/component'
 import { PrismaHelper } from '../helpers/prisma-helper'
 
 export class BusinessCanvasPrismaRepo implements AddBusinessCanvasRepo {
   async add (dto: BusinessCanvasModel): Promise<void> {
     const { components, createdAt, id, name, userId } = dto
     const prisma = await PrismaHelper.getCli()
-    await prisma.businessCanvas.create({ data: { id, name, createdAt, userId } })
-    const componentNames = Object.keys(components)
-    for (const componentName of componentNames) {
-      const key = componentName as ComponentName
-      await prisma.businessCanvasComponent.create({
-        data: { businessCanvasId: id, componentName, topics: components[key] }
+    await prisma.$transaction(async (transactionPrisma) => {
+      await transactionPrisma.businessCanvas.create({
+        data: { id, name, createdAt, userId }
       })
-    }
+      const componentEntries = Object.entries(components)
+      await transactionPrisma.businessCanvasComponent.createMany({
+        data: componentEntries.map(([componentName, topics]) => ({
+          businessCanvasId: id, componentName, topics
+        }))
+      })
+    })
   }
 }
