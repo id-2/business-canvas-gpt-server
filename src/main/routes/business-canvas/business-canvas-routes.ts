11ec15ccb134888dfa3e5b334db82da0719009e3
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 13:48:47 -0300

    feat: ensure business-canvas route call AccessControlMiddleware if x-access-token is provided

--- src/main/routes/business-canvas/business-canvas-routes.ts
+++ src/main/routes/business-canvas/business-canvas-routes.ts
@@ -1,9 +1,16 @@
-import { adaptRoute } from '@/main/adapters'
-import { createBusinessCanvasControllerFactory } from '@/main/factories/controllers/business-canvas/create-business-canvas-controller-factory'
 import type { Router } from 'express'
+import { adaptMiddleware, adaptRoute } from '@/main/adapters'
+import { createBusinessCanvasControllerFactory } from '@/main/factories/controllers/business-canvas/create-business-canvas-controller-factory'
+import { accessTokenVerifierMiddlewareDecoratorFactory } from '@/main/factories/decorators/access-token-verifier-middleware-decorator'
+import { accessControlMiddlewareFactory } from '@/main/factories/middlewares/access-control-middleware-factory'
 
 export default async (router: Router): Promise<void> => {
   router.post('/business-canvas',
+    adaptMiddleware(
+      accessTokenVerifierMiddlewareDecoratorFactory(
+        accessControlMiddlewareFactory('user')
+      )
+    ),
     adaptRoute(createBusinessCanvasControllerFactory())
   )
 }
