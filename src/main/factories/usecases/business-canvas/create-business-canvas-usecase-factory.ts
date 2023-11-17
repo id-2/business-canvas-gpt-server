b5ea3f81830514ed8d53c7a363ff301de4806817
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 02:31:01 -0300

    refactor: exchange the prisma for redis

--- src/main/factories/usecases/business-canvas/create-business-canvas-usecase-factory.ts
+++ src/main/factories/usecases/business-canvas/create-business-canvas-usecase-factory.ts
@@ -1,14 +1,14 @@
 import type { CreateBusinessCanvas } from '@/domain/contracts'
 import { CreateBusinessCanvasUseCase } from '@/interactions/usecases/business-canvas/create-business-canvas/create-business-canvas-usecase'
 import { businessCanvasOpenAiApiFactory } from '@/main/factories/infra/api/business-canvas-open-ai-api-factory'
-import { questionPrismaRepoFactory } from '@/main/factories/infra/db/prisma/question-prisma-repo-factory'
+import { questionRedisCacheFactory } from '../../infra/cache/question-redis-cache-factory'
 import { addManyAnswersUseCaseFactory } from '../answer/add-many-answers-usecase-factory'
 import { addRandomUserUseCaseFactory } from '../user/add-random-user-usecase-factory'
 import { addBusinessCanvasUseCaseFactory } from './add-many-answers-usecase-factory'
 
 export const createBusinessCanvasUseCaseFactory = (): CreateBusinessCanvas => {
   return new CreateBusinessCanvasUseCase(
-    questionPrismaRepoFactory(),
+    questionRedisCacheFactory(),
     addRandomUserUseCaseFactory(),
     addManyAnswersUseCaseFactory(),
     businessCanvasOpenAiApiFactory(),
