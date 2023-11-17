6c90c4a883aad9e3f1b03d73430171f608386ade
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 02:31:59 -0300

    fix: fix userId extraction error from request headers

--- src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
+++ src/presentation/controllers/create-business-canvas/create-business-canvas-controller.ts
@@ -1,7 +1,7 @@
-import type { Controller, Validation } from '@/presentation/contracts'
-import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
 import type { CreateBusinessCanvas } from '@/domain/contracts'
+import type { Controller, Validation } from '@/presentation/contracts'
 import { badRequest, created, serverError } from '@/presentation/helpers/http/http-helpers'
+import type { HttpRequest, HttpResponse } from '@/presentation/http/http'
 
 export class CreateBusinessCanvasController implements Controller {
   constructor (
@@ -15,7 +15,7 @@ export class CreateBusinessCanvasController implements Controller {
       if (validationResult.isLeft()) {
         return badRequest(validationResult.value)
       }
-      const { userId } = httpRequest.headers
+      const userId = httpRequest.headers?.userId
       const createBusinessCanvasResult = await this.createBusinessCanvas.perform({
         ...(userId && { userId }), answers: httpRequest.body
       })
