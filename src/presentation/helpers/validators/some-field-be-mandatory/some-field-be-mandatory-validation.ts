6b06e3cc4f0b15c3adb5d6d32fc8ad161d08597d
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 01:02:16 -0300

    feat: ensure SomeFieldBeMandatoryValidation return right result if any of the required fields are provided

--- src/presentation/helpers/validators/some-field-be-mandatory/some-field-be-mandatory-validation.ts
+++ src/presentation/helpers/validators/some-field-be-mandatory/some-field-be-mandatory-validation.ts
@@ -7,13 +7,17 @@ export class SomeFieldBeMandatoryValidation implements Validation {
 
   validate (input: any): Either<Error, null> {
     const keys = Object.keys(input)
+    let count = 0
     for (const field of this.requiredFields) {
-      if (!keys.some(key => key === field)) {
-        let fields = ''
-        this.requiredFields.forEach(field => { fields += `'${field}', ` })
-        fields = fields.slice(0, -2)
-        return left(new SomeFieldBeMandatoryError(fields))
+      if (keys.includes(field)) {
+        count++
       }
+    }
+    if (count === 0) {
+      let fields = ''
+      this.requiredFields.forEach(field => { fields += `'${field}', ` })
+      fields = fields.slice(0, -2)
+      return left(new SomeFieldBeMandatoryError(fields))
     }
     return right(null)
   }
