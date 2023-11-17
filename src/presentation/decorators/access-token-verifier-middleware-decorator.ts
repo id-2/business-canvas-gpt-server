a08c2211337773983c0e1bbd4497d5fd903818df
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 13:31:39 -0300

    feat: esnure AccessTokenVerifierMiddlewareDecorator not call Middleware if headers not provided

--- src/presentation/decorators/access-token-verifier-middleware-decorator.ts
+++ src/presentation/decorators/access-token-verifier-middleware-decorator.ts
@@ -5,7 +5,9 @@ export class AccessTokenVerifierMiddlewareDecorator implements Middleware {
   constructor (private readonly middleware: Middleware) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.middleware.handle(httpRequest)
+    if (httpRequest.headers) {
+      await this.middleware.handle(httpRequest)
+    }
     return { body: '', statusCode: 0 }
   }
 }
