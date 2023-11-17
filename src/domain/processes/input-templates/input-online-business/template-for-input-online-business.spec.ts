7093386f9aa596f8da4fe959cc371abb3af5877c
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 16:31:11 -0300

    test: ensure TemplateForInputOnlineBusiness return TemplateInputModel  that contains {{location}} and {{description}}

--- src/domain/processes/input-templates/input-online-business/template-for-input-online-business.spec.ts
+++ src/domain/processes/input-templates/input-online-business/template-for-input-online-business.spec.ts
@@ -6,4 +6,9 @@ describe('TemplateForInputOnlineBusiness', () => {
     expect(result.input).toBeTruthy()
     expect(typeof result.input).toBe('string')
   })
+
+  it('Should return TemplateInputModel that contains {{location}} and {{description}}', async () => {
+    const result = sut.create()
+    expect(result.input).toContain('{{description}}')
+  })
 })
