5abc6a974898053e76f4eb9b0eefe0f4bdee8546
mlucasdev <lucasmsantos819@gmail.com>
2023-11-17 12:35:52 -0300

    feat: remove AccessControlMiddleware at GET question route

--- src/main/routes/question/question-routes.ts
+++ src/main/routes/question/question-routes.ts
@@ -1,11 +1,9 @@
-import type { Router } from 'express'
-import { adaptMiddleware, adaptRoute } from '@/main/adapters'
+import { adaptRoute } from '@/main/adapters'
 import { fetchAllQuestionsControllerFactory } from '@/main/factories/controllers/question/fetch-all-questions-controller-factory'
-import { accessControlMiddlewareFactory } from '@/main/factories/middlewares/access-control-middleware-factory'
+import type { Router } from 'express'
 
 export default async (router: Router): Promise<void> => {
   router.get('/question',
-    adaptMiddleware(accessControlMiddlewareFactory('user')),
     adaptRoute(fetchAllQuestionsControllerFactory())
   )
 }
