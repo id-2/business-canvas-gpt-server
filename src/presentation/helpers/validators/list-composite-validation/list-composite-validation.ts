f315ed8afe0a1133ab2ff570695355a33a9edeec
mlucasdev <lucasmsantos819@gmail.com>
2023-11-16 23:55:28 -0300

    feat: ensure ListCompositeValidation return an error if any Validation fails

--- src/presentation/helpers/validators/list-composite-validation/list-composite-validation.ts
+++ src/presentation/helpers/validators/list-composite-validation/list-composite-validation.ts
@@ -1,5 +1,5 @@
 import { type Validation } from '@/presentation/contracts'
-import { right, type Either } from '@/shared/either'
+import { right, type Either, left } from '@/shared/either'
 
 export class ListCompositeValidation implements Validation {
   constructor (private readonly validations: Validation[]) {}
@@ -8,7 +8,10 @@ export class ListCompositeValidation implements Validation {
     for (const validation of this.validations) {
       const array = input as any[]
       for (const fields of array) {
-        validation.validate(fields)
+        const validationResult = validation.validate(fields)
+        if (validationResult.isLeft()) {
+          return left(validationResult.value)
+        }
       }
     }
     return right(null)
