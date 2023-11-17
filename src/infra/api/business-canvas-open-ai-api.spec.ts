1ca51ebe30afcf5ec95fb0dc99ee899dbf8499d9
mlucasdev <lucasmsantos819@gmail.com>
2023-11-16 23:11:51 -0300

    test: ensure BusinessCanvasOpenAiApi return an BusinessCanvasApiModel if OpenAi create is a success

--- src/infra/api/business-canvas-open-ai-api.spec.ts
+++ src/infra/api/business-canvas-open-ai-api.spec.ts
@@ -1,12 +1,49 @@
+import type { BusinessCanvasApiModel } from '@/domain/models/output-models'
 import { BusinessCanvasOpenAiApi } from './business-canvas-open-ai-api'
 import OpenAI from 'openai'
 
-jest.mock('openai')
+jest.mock('openai', () => ({
+  __esModule: true,
+  default: jest.fn().mockImplementation(() => ({
+    chat: {
+      completions: {
+        create: jest.fn().mockReturnValueOnce(
+          Promise.resolve({
+            choices: [{
+              message: {
+                content: makeFakeBusinessCanvasApiModel()
+              }
+            }]
+          })
+        )
+      }
+    }
+  }))
+}))
+
+const makeFakeBusinessCanvasApiModel = (): BusinessCanvasApiModel => ({
+  name: 'any_business_canvas_name',
+  customerSegments: ['any_customer_segments'],
+  valuePropositions: ['any_value_propositions'],
+  channels: ['any_channels'],
+  customerRelationships: ['any_customer_relationships'],
+  revenueStreams: ['any_revenue_streams'],
+  keyResources: ['any_key_resources'],
+  keyActivities: ['any_key_activities'],
+  keyPartnerships: ['any_key_partnerships'],
+  costStructure: ['any_cost_structure']
+})
 
 describe('BusinessCanvasOpenAi Api', () => {
   it('Should call OpenAI with correct key', async () => {
     const sut = new BusinessCanvasOpenAiApi()
     await sut.create({ input: 'any_input' })
     expect(OpenAI).toHaveBeenCalledWith({ apiKey: 'any_key' })
+  })
+
+  it('Should return an BusinessCanvasApiModel if OpenAi create is a success', async () => {
+    const sut = new BusinessCanvasOpenAiApi()
+    const result = await sut.create({ input: 'any_input' })
+    expect(result).toEqual(makeFakeBusinessCanvasApiModel())
   })
 })
